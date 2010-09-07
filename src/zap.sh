2ee550760d01e8fa0c4908f424392c5adab7f04f
psiinon <psiinon@gmail.com>
2010-09-07 19:34:46 +0000

    Fixed issue #1 - missing classpath

--- src/zap.sh
+++ src/zap.sh
@@ -1,1 +1,3 @@
-java -jar zap.jar
+CP="lib/commons-codec-1.2.jar:lib/commons-collections-3.1.jar:lib/commons-configuration-1.1.jar:lib/commons-httpclient-3.0.jar:lib/commons-lang-2.0.jar:lib/commons-logging-api.jar:lib/commons-logging.jar:lib/hsqldb.jar:lib/jh.jar:lib/js.jar:lib/log4j-1.2.8.jar:lib/zaphelp.jar:zap.jar"
+
+java -classpath $CP org.zaproxy.zap.ZAP
