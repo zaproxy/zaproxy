df189ebbc3f32569fd4bb78d17088a46e0b2e5c0
psiinon <psiinon@gmail.com>
2010-12-04 09:48:04 +0000

    Include new jars

--- src/zap.sh
+++ src/zap.sh
@@ -1,3 +1,3 @@
-CP="lib/commons-codec-1.2.jar:lib/commons-collections-3.1.jar:lib/commons-configuration-1.1.jar:lib/commons-httpclient-3.0.jar:lib/commons-lang-2.0.jar:lib/commons-logging-api.jar:lib/commons-logging.jar:lib/hsqldb.jar:lib/jh.jar:lib/js.jar:lib/log4j-1.2.8.jar:lib/zaphelp.jar:zap.jar"
+CP="lib/BrowserLauncher2-1_3.jar:lib/commons-codec-1.2.jar:lib/commons-collections-3.1.jar:lib/commons-configuration-1.1.jar:lib/commons-httpclient-3.0.jar:lib/commons-lang-2.0.jar:lib/commons-logging-api.jar:lib/commons-logging.jar:lib/hsqldb.jar:lib/java-getopt-1.0.13.jar:lib/jdom.jar:lib/jericho-html-2.6.jar:lib/jh.jar:lib/js.jar:lib/log4j-1.2.8.jar:lib/looks-2.2.0.jar:lib/swing-layout-1.0.3.jar:lib/zaphelp.jar:zap.jar"
 
 java -classpath $CP org.zaproxy.zap.ZAP
