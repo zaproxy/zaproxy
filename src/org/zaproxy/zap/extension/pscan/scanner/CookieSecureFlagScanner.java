1634867d9b8a736e6cfe7d121a75b5bd358eea29
a.c.neumann <a.c.neumann@gmail.com>
2011-01-04 19:03:24 +0000

    Plugin for checking the secure flag using the passive scanner

---
+++ src/org/zaproxy/zap/extension/pscan/scanner/CookieSecureFlagScanner.java
@@ -0,0 +1,83 @@
+package org.zaproxy.zap.extension.pscan.scanner;
+
+import java.util.Vector;
+
+import net.htmlparser.jericho.Source;
+
+import org.parosproxy.paros.core.scanner.Alert;
+import org.parosproxy.paros.network.HttpHeader;
+import org.parosproxy.paros.network.HttpMessage;
+import org.zaproxy.zap.extension.pscan.PassiveScanThread;
+import org.zaproxy.zap.extension.pscan.PassiveScanner;
+
+
+public class CookieSecureFlagScanner implements PassiveScanner {
+
+	private PassiveScanThread parent = null;
+	//private Logger logger = Logger.getLogger(this.getClass());
+
+	@Override
+	public void setParent (PassiveScanThread parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public void scanHttpRequestSend(HttpMessage msg, int id) {
+		// Ignore
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
+		if (!msg.getRequestHeader().getSecure()) {
+			// If SSL isn't used then the Secure flag has not to be checked
+			return;
+		}
+		
+		Vector<String> cookies1 = msg.getResponseHeader().getHeaders(HttpHeader.SET_COOKIE);
+
+		if (cookies1 != null) {
+			for (String cookie : cookies1) {
+				if (cookie.toLowerCase().indexOf("secure") < 0) {
+					this.raiseAlert(msg, id, cookie);
+				}
+			}
+		}
+
+		Vector<String> cookies2 = msg.getResponseHeader().getHeaders(HttpHeader.SET_COOKIE2);
+		
+		if (cookies2 != null) {
+			for (String cookie : cookies2) {
+				if (cookie.toLowerCase().indexOf("secure") < 0) {
+					this.raiseAlert(msg, id, cookie);
+				}
+			}
+		}
+	}
+	
+	private void raiseAlert(HttpMessage msg, int id, String cookie) {
+	    Alert alert = new Alert(getId(), Alert.RISK_LOW, Alert.WARNING, 
+		    	"Cookie set without secure flag");
+		    	alert.setDetail(
+		    	    "A cookie has been set without the secure flag, which means that the cookie can be accessed via unencrypted connections.", 
+		    	    msg.getRequestHeader().getURI().toString(),
+		    	    cookie, "", 
+		    	    "Whenever a cookie contains sensitive information or is a session token, then it should always be passed using an encrypted tunnel." +
+                            "Ensure that the secure flag is set for cookies containing such sensitive information.", 
+		            "http://www.owasp.org/index.php/Testing_for_cookies_attributes_(OWASP-SM-002)", 
+		            msg);
+	
+    	parent.raiseAlert(id, alert);
+
+	}
+
+	private int getId() {
+		return 40003;
+	}
+
+	@Override
+	public String getName() {
+		return "Cookie without secure flag";
+	}
+
+}
