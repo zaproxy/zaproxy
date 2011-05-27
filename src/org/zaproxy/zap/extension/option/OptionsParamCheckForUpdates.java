19af7ddb6cc79d9b9bfdd584f7b09daf38fe7cb0
psiinon <psiinon@gmail.com>
2011-05-27 13:09:28 +0000

    Only checkforupdates once per day

--- src/org/zaproxy/zap/extension/option/OptionsParamCheckForUpdates.java
+++ src/org/zaproxy/zap/extension/option/OptionsParamCheckForUpdates.java
@@ -19,20 +19,25 @@
  */
 package org.zaproxy.zap.extension.option;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.parosproxy.paros.common.AbstractParam;
 
-/**
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 public class OptionsParamCheckForUpdates extends AbstractParam {
 
 	public static final String CHECK_ON_START = "start.checkForUpdates";
+	public static final String DAY_LAST_CHECKED = "start.dayLastChecked";
 	
 	private int checkOnStart = 0;
+	// Day last checked is used to ensure if the user has agreed then we only check the first time ZAP is run every day
+	private String dayLastChecked = null; 
 	private boolean unset = true;
-	
+    private static Log log = LogFactory.getLog(OptionsParamCheckForUpdates.class);
+
     /**
      * @param rootElementName
      */
@@ -45,7 +50,10 @@ public class OptionsParamCheckForUpdates extends AbstractParam {
     protected void parse() {
         
 	    checkOnStart = getConfig().getInt(CHECK_ON_START, 0);
-		unset = getConfig().getString(CHECK_ON_START, "").equals("");
+	    dayLastChecked = getConfig().getString(DAY_LAST_CHECKED, "");
+	    // There was a bug in 1.2.0 where it defaulted silently to dont check
+	    // We now use the lack of a dayLastChecked value to indicate we should reprompt the user.
+		unset = getConfig().getString(CHECK_ON_START, "").equals("") || dayLastChecked.length() == 0;
     }
 
 	/**
@@ -66,9 +74,30 @@ public class OptionsParamCheckForUpdates extends AbstractParam {
 	public void setChckOnStart(int checkOnStart) {
 		this.checkOnStart = checkOnStart;
 		getConfig().setProperty(CHECK_ON_START, Integer.toString(checkOnStart));
+		if (dayLastChecked.length() == 0) {
+			dayLastChecked = "Never";
+			getConfig().setProperty(DAY_LAST_CHECKED, dayLastChecked);
+		}
 	}
 	
 	public boolean isCheckOnStart() {
-		return !(checkOnStart == 0);
+		if (checkOnStart == 0) {
+			log.debug("isCheckForStart - false");
+			return false;
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String today = sdf.format(new Date());
+		if (today.equals(dayLastChecked)) {
+			log.debug("isCheckForStart - already checked today");
+			return false;
+		}
+		getConfig().setProperty(DAY_LAST_CHECKED, today);
+		try {
+			getConfig().save();
+		} catch (ConfigurationException e) {
+			log.error(e.getMessage(), e);
+		}
+		
+		return true;
 	}
 }
