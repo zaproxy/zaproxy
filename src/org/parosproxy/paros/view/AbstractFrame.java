a948ca19501b57df4252cfd0f694fb0beae917f8
nitram509 <mawoki@ymail.com>
2011-05-30 20:24:08 +0000

    switched to icon List lazy load for icon images

--- src/org/parosproxy/paros/view/AbstractFrame.java
+++ src/org/parosproxy/paros/view/AbstractFrame.java
@@ -2,19 +2,19 @@
  * Created on May 17, 2004
  *
  * Paros and its related class files.
- * 
+ *
  * Paros is an HTTP/HTTPS proxy for assessing web application security.
  * Copyright (C) 2003-2004 Chinotec Technologies Company
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the Clarified Artistic License
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * Clarified Artistic License for more details.
- * 
+ *
  * You should have received a copy of the Clarified Artistic License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -22,7 +22,10 @@
 package org.parosproxy.paros.view;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
@@ -44,6 +47,8 @@ public abstract class AbstractFrame extends JFrame {
 
 	private static final long serialVersionUID = 7658476500356416014L;
 
+	private List<Image> icons = null;
+
 	/**
 	 * This is the default constructor
 	 */
@@ -53,17 +58,17 @@ public abstract class AbstractFrame extends JFrame {
 	}
 	/**
 	 * This method initializes this
-	 * 
+	 *
 	 * @return void
 	 */
 	private void initialize() {
 		// ZAP: Rebrand
-		this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resource/zap48x48.png")));
-		
+		this.setIconImages(loadIconImages());
+
 		this.setVisible(false);
 		this.setTitle(Constant.PROGRAM_NAME);
 	    this.setFont(new java.awt.Font("Dialog", java.awt.Font.PLAIN, 11));
-	    
+
 	    if (Model.getSingleton().getOptionsParam().getViewParam().getWmUiHandlingOption() == 0) {
 	    	this.setSize(800, 600);
 	    	centerFrame();
@@ -74,8 +79,8 @@ public abstract class AbstractFrame extends JFrame {
 	 *
 	 */
 	public void centerFrame() {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = this.getSize();
+		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		final Dimension frameSize = this.getSize();
 		if (frameSize.height > screenSize.height) {
 			frameSize.height = screenSize.height;
 		}
@@ -84,4 +89,21 @@ public abstract class AbstractFrame extends JFrame {
 		}
 	    this.setLocation((screenSize.width - frameSize.width) / 2, (screenSize.height - frameSize.height) / 2);
 	}
+
+	protected List<Image> loadIconImages() {
+		if (icons == null) {
+			icons = new ArrayList<Image>();
+			icons.add(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resource/zap16x16.png")));
+			icons.add(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resource/zap48x48.png")));
+		}
+		return icons;
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		this.icons.clear(); // free the bounded resources
+		this.icons = null;
+	}
+
 }  //  @jve:visual-info  decl-index=0 visual-constraint="31,17"
