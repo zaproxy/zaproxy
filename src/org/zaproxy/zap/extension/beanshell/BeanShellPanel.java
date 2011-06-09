048c00cddf2d810ecfdeee3bfa21af58d18791e9
nitram509 <mawoki@ymail.com>
2011-06-09 21:20:15 +0000

    Issue 79 Fixed splitterSize in BeanShellPanel

--- src/org/zaproxy/zap/extension/beanshell/BeanShellPanel.java
+++ src/org/zaproxy/zap/extension/beanshell/BeanShellPanel.java
@@ -77,7 +77,7 @@ public class BeanShellPanel extends AbstractPanel {
 		GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
 
 		this.setLayout(new GridBagLayout());
-		this.setSize(403, 296);
+		this.setPreferredSize(new Dimension(400, 300));
 		gridBagConstraints1.gridx = 0;
 		gridBagConstraints1.gridy = 0;
 		gridBagConstraints1.weightx = 1.0;
@@ -102,7 +102,7 @@ public class BeanShellPanel extends AbstractPanel {
 	private JSplitPane getSplitVert() {
 		if (splitVert == null) {
 			splitVert = new JSplitPane();
-			splitVert.setDividerLocation(220);
+			splitVert.setDividerLocation(0.5d);
 			splitVert.setDividerSize(3);
 			splitVert.setOrientation(JSplitPane.VERTICAL_SPLIT);
 			splitVert.setPreferredSize(new Dimension(400,400));
