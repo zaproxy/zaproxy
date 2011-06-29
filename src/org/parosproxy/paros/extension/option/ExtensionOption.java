54dc0ff47d9ed1bf466258acb8066c7624496036
a.c.neumann <a.c.neumann@gmail.com>
2011-06-29 11:56:09 +0000

    Added language pack support for the 1.3 branch

--- src/org/parosproxy/paros/extension/option/ExtensionOption.java
+++ src/org/parosproxy/paros/extension/option/ExtensionOption.java
@@ -26,13 +26,10 @@ import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.extension.ExtensionAdaptor;
 import org.parosproxy.paros.extension.ExtensionHook;
 import org.parosproxy.paros.extension.ExtensionHookView;
+import org.zaproxy.zap.extension.lang.OptionsLangPanel;
 import org.zaproxy.zap.extension.option.OptionsCheckForUpdatesPanel;
 
-/**
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+
 public class ExtensionOption extends ExtensionAdaptor {
 
 	private JCheckBoxMenuItem menuViewImage = null;
@@ -42,32 +39,22 @@ public class ExtensionOption extends ExtensionAdaptor {
 	private OptionsLocalProxyPanel optionsLocalProxyPanel = null;
 	private OptionsViewPanel optionsViewPanel = null;
 	private OptionsCheckForUpdatesPanel optionsCheckForUpdatesPanel = null;
-    /**
-     * 
-     */
+	private OptionsLangPanel optionsLangPanel = null;
+
+	
     public ExtensionOption() {
         super();
  		initialize();
     }
 
-    /**
-     * @param name
-     */
     public ExtensionOption(String name) {
         super(name);
     }
 
-	/**
-	 * This method initializes this
-	 * 
-	 * @return void
-	 */
 	private void initialize() {
         this.setName("ExtensionViewOption");
-			
 	}
 	
-
 	public void hook(ExtensionHook extensionHook) {
 	    super.hook(extensionHook);
 	    if (getView() != null) {
@@ -79,92 +66,71 @@ public class ExtensionOption extends ExtensionAdaptor {
 	        extensionHook.getHookView().addOptionPanel(getOptionsAuthenticationPanel());
 	        extensionHook.getHookView().addOptionPanel(getOptionsCertificatePanel());
 	        extensionHook.getHookView().addOptionPanel(getOptionsViewPanel());
-	        // ZAP: Added Check for Updates options panel
 	        extensionHook.getHookView().addOptionPanel(getOptionsCheckForUpdatesPanel());
+	        extensionHook.getHookView().addOptionPanel(getOptionsLangPanel());
 	    }
 	}
 
-	
-	/**
-	 * This method initializes menuViewImage	
-	 * 	
-	 * @return javax.swing.JCheckBoxMenuItem	
-	 */    
 	private JCheckBoxMenuItem getMenuViewImage() {
 		if (menuViewImage == null) {
 			menuViewImage = new JCheckBoxMenuItem();
 			menuViewImage.setText(Constant.messages.getString("menu.view.enableImage"));	// ZAP: i18n
 			menuViewImage.addItemListener(new java.awt.event.ItemListener() { 
-
 				public void itemStateChanged(java.awt.event.ItemEvent e) {    
-
-					getModel().getOptionsParam().getViewParam().setProcessImages(getMenuViewImage().getState() ? 1 : 0);
-					
+					getModel().getOptionsParam().getViewParam().setProcessImages(getMenuViewImage().getState() ? 1 : 0);					
 				}
 			});
 
 		}
 		return menuViewImage;
 	}
-	/**
-	 * This method initializes optionsConnectionPanel	
-	 * 	
-	 * @return org.parosproxy.paros.extension.viewOption.OptionsConnectionPanel	
-	 */    
+ 
 	private OptionsConnectionPanel getOptionsConnectionPanel() {
 		if (optionsConnectionPanel == null) {
 			optionsConnectionPanel = new OptionsConnectionPanel();
 		}
 		return optionsConnectionPanel;
 	}
-	/**
-	 * This method initializes optionsAuthenticationPanel	
-	 * 	
-	 * @return org.parosproxy.paros.extension.viewOption.OptionsAuthenticationPanel	
-	 */    
+
 	private OptionsAuthenticationPanel getOptionsAuthenticationPanel() {
 		if (optionsAuthenticationPanel == null) {
 			optionsAuthenticationPanel = new OptionsAuthenticationPanel();
 		}
 		return optionsAuthenticationPanel;
 	}
-	/**
-	 * This method initializes optionsCertificatePanel	
-	 * 	
-	 * @return org.parosproxy.paros.extension.viewOption.OptionsCertificatePanel	
-	 */    
+
 	private OptionsCertificatePanel getOptionsCertificatePanel() {
 		if (optionsCertificatePanel == null) {
 			optionsCertificatePanel = new OptionsCertificatePanel();
 		}
 		return optionsCertificatePanel;
 	}
-	/**
-	 * This method initializes optionsLocalProxyPanel	
-	 * 	
-	 * @return org.parosproxy.paros.extension.viewOption.OptionsLocalProxyPanel	
-	 */    
+   
 	private OptionsLocalProxyPanel getOptionsLocalProxyPanel() {
 		if (optionsLocalProxyPanel == null) {
 			optionsLocalProxyPanel = new OptionsLocalProxyPanel();
 		}
 		return optionsLocalProxyPanel;
 	}
-	/**
-	 * This method initializes optionsViewPanel	
-	 * 	
-	 * @return org.parosproxy.paros.extension.viewOption.OptionsViewPanel	
-	 */    
+  
 	private OptionsViewPanel getOptionsViewPanel() {
 		if (optionsViewPanel == null) {
 			optionsViewPanel = new OptionsViewPanel();
 		}
 		return optionsViewPanel;
 	}
+	
 	private OptionsCheckForUpdatesPanel getOptionsCheckForUpdatesPanel() {
 		if (optionsCheckForUpdatesPanel == null) {
 			optionsCheckForUpdatesPanel = new OptionsCheckForUpdatesPanel();
 		}
 		return optionsCheckForUpdatesPanel;
+	}
+	
+	private OptionsLangPanel getOptionsLangPanel() {
+		if (optionsLangPanel == null) {
+			optionsLangPanel = new OptionsLangPanel();
+		}
+		return optionsLangPanel;
 	}
 }
