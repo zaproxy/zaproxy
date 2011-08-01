a9c2b0d577aed093fb8de8370d98946580628ad3
nitram509 <mawoki@ymail.com>
2011-08-01 19:44:51 +0000

    Merged r753,r754 and thus merged this fix into 1.3 branch
    Update Issue 138

--- src/org/parosproxy/paros/network/HttpInputStream.java
+++ src/org/parosproxy/paros/network/HttpInputStream.java
@@ -60,21 +60,14 @@ public class HttpInputStream extends BufferedInputStream {
 		String msg = "";
         int		oneByte = -1;
         boolean eoh = false;
-        boolean neverReadOnce = true;
-        StringBuffer sb = new StringBuffer(200);
+        StringBuilder sb = new StringBuilder(200);
         
         do {
             oneByte = super.read();
         	
         	if (oneByte == -1) {
         		eoh = true;
-        		if (neverReadOnce) {
-        			HttpUtil.sleep(50);
-        			continue;
-        		}
 				break;
-        	} else {
-        		neverReadOnce = false;
         	}
             sb.append((char) oneByte);
 
@@ -82,7 +75,7 @@ public class HttpInputStream extends BufferedInputStream {
                 eoh = true;
                 msg = sb.toString();
             }
-		} while (!eoh || neverReadOnce);
+		} while (!eoh);
 
         return msg;
 
@@ -93,7 +86,7 @@ public class HttpInputStream extends BufferedInputStream {
 	 * @param sb
 	 * @return true - if end of HTTP header.
 	 */
-	private boolean isHeaderEnd(StringBuffer sb) {
+	private static final boolean isHeaderEnd(StringBuilder sb) {
 		int len = sb.length();
 		if (len > 2) {
 			if (LF2.equals(sb.substring(len-2))) {
