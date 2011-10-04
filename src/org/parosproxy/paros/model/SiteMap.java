5e0bee101b5c95e69707b7d9e2b182ceef48bc0e
thc202 <thc202@gmail.com>
2011-10-04 14:52:27 +0000

    Bugfix Issue 191 - Exception when the URL contains escaped characters

--- src/org/parosproxy/paros/model/SiteMap.java
+++ src/org/parosproxy/paros/model/SiteMap.java
@@ -541,7 +541,8 @@ public class SiteMap extends DefaultTreeModel {
         }
         HttpMessage newMsg = base.cloneRequest();
         
-        URI uri = new URI(sb.toString(), true);
+        // ZAP: Prevents a possible URIException, because the passed string is not escaped.
+        URI uri = new URI(sb.toString(), false);
         newMsg.getRequestHeader().setURI(uri);
         newMsg.getRequestHeader().setMethod(HttpRequestHeader.GET);
         newMsg.getRequestBody().setBody("");
