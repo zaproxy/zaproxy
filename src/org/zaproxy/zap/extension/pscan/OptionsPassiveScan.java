7d8c31312ffbb6c3edbb3cc4d3b54b781bf50998
psiinon <psiinon@gmail.com>
2012-03-19 20:48:03 +0000

    Issue 287: Passive scanner doesnt pick up new anticsrf tokens

--- src/org/zaproxy/zap/extension/pscan/OptionsPassiveScan.java
+++ src/org/zaproxy/zap/extension/pscan/OptionsPassiveScan.java
@@ -337,7 +337,7 @@ public class OptionsPassiveScan extends AbstractParamPanel {
 	        				RegexAutoTagScanner rats = (RegexAutoTagScanner) defn;
 	        				editName.setText(rats.getName());
 	        				editType.setText(rats.getType().name());
-	        				editConfig.setText(rats.getConfig());
+	        				editConfig.setText(rats.getConf());
 	        		    	editRequestUrlRegex.setText(rats.getRequestUrlRegex());
 	        		    	editRequestHeaderRegex.setText(rats.getRequestHeaderRegex());
 	        		    	editResponseHeaderRegex.setText(rats.getResponseHeaderRegex());
@@ -379,8 +379,8 @@ public class OptionsPassiveScan extends AbstractParamPanel {
 					editName.getText(), RegexAutoTagScanner.TYPE.TAG, editConfig.getText());
 			isNew = true;
 		}
-		// TODO validate params, eg config is mandatory
-		rats.setConfig(editConfig.getText());
+		// TODO validate params, eg conf is mandatory
+		rats.setConf(editConfig.getText());
 		rats.setRequestHeaderRegex(editRequestHeaderRegex.getText());
 		rats.setRequestUrlRegex(editRequestUrlRegex.getText());
 		rats.setResponseHeaderRegex(editResponseHeaderRegex.getText());
