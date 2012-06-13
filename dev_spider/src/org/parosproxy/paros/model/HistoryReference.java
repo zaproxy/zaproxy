9e72f480cc9cf750ce932130f2894b1f8afea0c0
cosminstefanxp <cosminstefanxp@gmail.com>
2012-06-13 21:47:07 +0000

    Fixed some things in HistoryReference.

--- dev_spider/src/org/parosproxy/paros/model/HistoryReference.java
+++ dev_spider/src/org/parosproxy/paros/model/HistoryReference.java
@@ -304,10 +304,10 @@ public class HistoryReference {
 	   
 	   //If this is the first alert
 	   if(alerts==null)
-		   alerts=new ArrayList<Alert>();
+		   alerts=new ArrayList<Alert>(1);
 	   
 	   for (Alert a : alerts) {
-		   if (a.getAlertId()==alert.getAlertId()) {
+		   if (alert.equals(a)) {
 			   // We've already recorded it
 			   return false;
 		   }
