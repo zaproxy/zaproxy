e6c48f0ab8128cbef65734dfbac8e47f3f78f87b
psiinon <psiinon@gmail.com>
2012-07-04 13:46:39 +0000

    Issue 252: 	Python API client - thanks to Steve Milner (ashcrow)

---
+++ python/api/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Standard build script.
+"""
+
+__docformat__ = 'restructuredtext'
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print "You must have setuptools installed to use setup.py. Exiting..."
+    raise SystemExit(1)
+
+
+setup(
+    name="python-owasp-zap",
+    version="0.0.1",
+    description="OWASP ZAP API client",
+    long_description="OWASP Zed Attack Proxy API python client",
+    author="ZAP development team",
+    author_email='',
+    url="https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project",
+    download_url="http://code.google.com/p/zaproxy/downloads/list",
+    platforms=['any'],
+
+    license="ASL2.0",
+
+    package_dir={
+        '': 'src',
+    },
+    packages=find_packages('src'),
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 4 - Beta',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python'],
+)
