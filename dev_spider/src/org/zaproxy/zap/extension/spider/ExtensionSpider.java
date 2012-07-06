7896ab2d379dc724c32e7ba630ea54eeef7789bb
cosminstefanxp <cosminstefanxp@gmail.com>
2012-07-06 12:18:41 +0000

    Small commit.

--- dev_spider/src/org/zaproxy/zap/extension/spider/ExtensionSpider.java
+++ dev_spider/src/org/zaproxy/zap/extension/spider/ExtensionSpider.java
@@ -67,7 +67,7 @@ public class ExtensionSpider extends ExtensionAdaptor
 	private List<String> excludeList = null;
     
 	/**
-     * 
+     *  
      */
     public ExtensionSpider() {
         super();
