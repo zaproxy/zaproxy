ea34c91e71be1b5201c6e6c478f23cc274a8a1f9
a.c.neumann <a.c.neumann@gmail.com>
2012-10-09 17:58:15 +0000

    Adapted Shell script to read memory from procfs for Linux Adapted Shell script to be Mac OS X compatible Deleted unnecessary Java directory from the Mac bundle

--- src/zap.sh
+++ src/zap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #Dereference from link to the real directory
 SCRIPTNAME=$0
@@ -6,23 +6,33 @@ SCRIPTNAME=$0
 #While name of this script is symbolic link
 while [ -L "$SCRIPTNAME" ] ; do 
         #Dereference the link to the name of the link target 
-        SCRIPTNAME=`ls -l "$SCRIPTNAME" | awk '{ print $NF; }'`
+        SCRIPTNAME=$(ls -l "$SCRIPTNAME" | awk '{ print $NF; }')
 done
 
 #Base directory where ZAP is installed
-BASEDIR=`dirname $SCRIPTNAME`
+BASEDIR=$(dirname "$SCRIPTNAME")
 
 #Switch to the directory where ZAP is installed
 cd "$BASEDIR"
 
+#Get Operating System
+OS=$(uname -s)
+
 #Work out best memory options
-MEM=`free -m | grep Mem | awk '{ print $2;}'`
+if [ "$OS" = "Linux" ]; then
+	MEM=$(expr $(sed -n 's/MemTotal:[ ]\{1,\}\([0-9]\{1,\}\) kB/\1/p' /proc/meminfo) / 1024)
+elif [ "$OS" = "Darwin" ]; then
+	MEM=$(system_profiler SPMemoryDataType | sed -n -e 's/.*Size: \([0-9]\{1,\}\) GB/\1/p' | awk '{s+=$0} END {print s*1024}')
+else
+    echo "Sorry, your plaform is not supported yet! You'll have to start ZAP from the commandline."
+    exit 1;
+fi
 
 if [ -z $MEM ]
 then
 	echo "Failed to obtain current memory, using jmv default memory settings"
 else
-	echo "Available memory: " $MEM"Mb"
+	echo "Available memory: " $MEM "MB"
 	if [ $MEM -gt 1500 ]
 	then
 		JMEM="-Xmx512m"
@@ -45,5 +55,6 @@ then
 fi
 
 #Start ZAP
-java ${JMEM} -XX:PermSize=256M -jar ${BASEDIR}/zap.jar org.zaproxy.zap.ZAP $*
+
+java ${JMEM} -XX:PermSize=256M -jar "${BASEDIR}/zap.jar" org.zaproxy.zap.ZAP $*
 
