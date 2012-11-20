79ba9f87b73cd83b471f39fbee2ee4463293598c
bkimminich <bjoern.kimminich@gmx.de>
2012-11-20 02:33:22 +0000

    Added unit test for Util xlass

--- test/org/zaproxy/ZaproxyUnitTestSuite.java
+++ test/org/zaproxy/ZaproxyUnitTestSuite.java
@@ -3,6 +3,7 @@ package org.zaproxy;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.parosproxy.paros.core.scanner.UtilUnitTest;
 import org.zaproxy.zap.spider.URLResolverUnitTest;
 
 import ch.csnc.extension.httpclient.AliasKeyManagerUnitTest;
@@ -11,11 +12,11 @@ import ch.csnc.extension.httpclient.AliasKeyManagerUnitTest;
  * Suite for all unit tests of ZAP. Tests attached to this suite must run
  * independent of any environmental settings or preferences on any possible
  * machine.
- * 
+ *
  * @author bjoern.kimminich@gmx.de
  */
 @RunWith(Suite.class)
-@SuiteClasses({ URLResolverUnitTest.class, AliasKeyManagerUnitTest.class })
+@SuiteClasses({ URLResolverUnitTest.class, AliasKeyManagerUnitTest.class, UtilUnitTest.class })
 public class ZaproxyUnitTestSuite {
 
 }
