8929b43cbd6562d83efbe789675090991b3bcbab
bkimminich <bjoern.kimminich@gmx.de>
2012-12-22 13:28:58 +0000

    Removed some more redundancies, renamed messages, removed obsolete inline comments

--- src/com/sittinglittleduck/DirBuster/CheckForUpdates.java
+++ src/com/sittinglittleduck/DirBuster/CheckForUpdates.java
@@ -35,27 +35,34 @@ import edu.stanford.ejalbert.exception.BrowserLaunchingInitializingException;
 import edu.stanford.ejalbert.exception.UnsupportedOperatingSystemException;
 
 /**
- * 
  * @author James
  */
 public class CheckForUpdates implements Runnable {
 
 	/*
-	 * Format for the expected xml
-	 * 
-	 * <version current="x.x.x-xxxx"/> <changelog> text...................
-	 * <changelog>
+	 * Format for the expected XML:
+	 * <version current="x.x.x-xxxx"/><changelog>text...........</changelog>
 	 */
+	private static final Pattern VERSION_PATTERN = Pattern
+			.compile("<version current=\\\"(.*?)\\\"/>");
+	private static final Pattern CHANGELOG_PATTERN = Pattern.compile(
+			"<changelog>(.*?)</changelog>", Pattern.DOTALL);
+
+	private static final String DIRBUSTER_SOURCEFORGE_URL = "https://sourceforge.net/project/showfiles.php?group_id=199126";
 
 	private static final String RETRY_MESSAGE = "Please try again later";
-	
-	private static final String CHANGELOG_ERROR_MESSAGE = "Sorry it has not been possible to check if this the latest version\n\n"+RETRY_MESSAGE;
-	private static final String BROWSER_ERROR_MESSAGE = "Sorry it has not been possible to open your browser to get the latest version\n\n";
 	private static final String DEFAULT_ERROR_MESSAGE = "Sorry there has been an error while checking for the latest version\n\n";
-	private static final String RETRY_LATER_ERROR_MESSAGE = DEFAULT_ERROR_MESSAGE + RETRY_MESSAGE;
-	/*
-	 * instance of the manager
-	 */
+
+	private static final String CHANGELOG_ERROR_MESSAGE = "Sorry it has not been possible to check if this the latest version\n\n"
+			+ RETRY_MESSAGE;
+	private static final String BROWSER_ERROR_MESSAGE = "Sorry it has not been possible to open your browser to get the latest version\n\n";
+	private static final String VERSION_ERROR_MESSAGE = DEFAULT_ERROR_MESSAGE
+			+ RETRY_MESSAGE;
+	private static final String OTHER_ERROR_MESSAGE = DEFAULT_ERROR_MESSAGE
+			+ RETRY_MESSAGE;
+	private static final String SERVER_ERROR_MESSAGE = DEFAULT_ERROR_MESSAGE
+			+ RETRY_MESSAGE;
+
 	private Manager manager;
 	private HttpClient httpclient;
 	String updateURL = "http://www.sittinglittleduck.com/DirBuster/checkForUpdate.php?version="
@@ -70,9 +77,6 @@ public class CheckForUpdates implements Runnable {
 
 	public void run() {
 		try {
-			/*
-			 * query the site to find out is there any updates
-			 */
 			GetMethod httpget = new GetMethod(updateURL);
 			int responseCode = httpclient.executeMethod(httpget);
 
@@ -80,82 +84,37 @@ public class CheckForUpdates implements Runnable {
 				if (httpget.getResponseContentLength() > 0) {
 
 					// get the http body
-					BufferedReader input = new BufferedReader(
+					String response = "";
+					try (BufferedReader input = new BufferedReader(
 							new InputStreamReader(
-									httpget.getResponseBodyAsStream()));
+									httpget.getResponseBodyAsStream()))) {
+						String line;
 
-					String line;
-
-					String responce = "";
-
-					StringBuffer buf = new StringBuffer();
-					while ((line = input.readLine()) != null) {
-						buf.append("\r\n" + line);
+						StringBuffer buf = new StringBuffer();
+						while ((line = input.readLine()) != null) {
+							buf.append("\r\n" + line);
+						}
+						response = buf.toString();
 					}
-					responce = buf.toString();
-					input.close();
 
-					/*
-					 * extract the data from the response
-					 */
-					String versionRegex = "<version current=\\\"(.*?)\\\"/>";
-					String changeLogRegex = "<changelog>(.*?)</changelog>";
-
-					Pattern regexFindFile = Pattern.compile(versionRegex);
-
-					Matcher m = regexFindFile.matcher(responce);
-
-					if (m.find()) {
-						String latestversion = m.group(1);
+					Matcher versionMatcher = VERSION_PATTERN.matcher(response);
+					if (versionMatcher.find()) {
+						String latestversion = versionMatcher.group(1);
 						if (latestversion.equalsIgnoreCase("Running - latest")) {
-							/*
-							 * no updates needed
-							 */
-							if (informUser) {
-								JOptionPane.showMessageDialog(manager.gui,
-										"You are running the latest version",
-										"You are running the latest version",
-										JOptionPane.INFORMATION_MESSAGE);
-							}
+							showAlreadyLatestVersionMessage();
 						} else {
-							/*
-							 * This is not the latest version of Dirbuster
-							 */
-							Pattern regexFindChangeLog = Pattern.compile(
-									changeLogRegex, Pattern.DOTALL);
-
-							m = regexFindChangeLog.matcher(responce);
-
-							if (m.find()) {
-								String changelog = m.group(1);
+							Matcher changelogMatcher = CHANGELOG_PATTERN
+									.matcher(response);
+							if (changelogMatcher.find()) {
+								String changelog = changelogMatcher.group(1);
 
 								if (!manager.isHeadLessMode()) {
-									/*
-									 * display pop to user about the fact there
-									 * is a new version
-									 */
-									int n = JOptionPane
-											.showConfirmDialog(
-													manager.gui,
-													"A new version of DirBuster ("
-															+ latestversion
-															+ ") is available\n\n"
-															+ "Change log:\n\n"
-															+ changelog
-															+ "\n\n"
-															+ "Do you wish to get the new version now?\n\n"
-															+ "(Auto checking can be disabled from 'Advanced Options -> DirBuster Options')",
-													"A new version of DirBuster is Avaliable",
-													JOptionPane.OK_CANCEL_OPTION);
-									// if the anwser is ok
-									if (n == 0) {
+									if (showUpdateToLatestVersionConfirmDialog(
+											latestversion, changelog) == JOptionPane.OK_OPTION) {
 										BrowserLauncher launcher;
 										try {
-											/*
-											 * open the sourceforge page
-											 */
 											launcher = new BrowserLauncher(null);
-											launcher.openURLinBrowser("https://sourceforge.net/project/showfiles.php?group_id=199126");
+											launcher.openURLinBrowser(DIRBUSTER_SOURCEFORGE_URL);
 										} catch (
 												BrowserLaunchingInitializingException
 												| UnsupportedOperatingSystemException ex) {
@@ -167,36 +126,50 @@ public class CheckForUpdates implements Runnable {
 									printUpdateMessageOnConsole(latestversion);
 								}
 							} else {
-								/*
-								 * error cant find the changelog
-								 */
 								showErrorMessage(CHANGELOG_ERROR_MESSAGE);
 							}
 
 						}
 					} else {
-						/*
-						 * error can't find the version
-						 */
-						showErrorMessage(RETRY_LATER_ERROR_MESSAGE);
+						showErrorMessage(VERSION_ERROR_MESSAGE);
 					}
 
 				} else {
-					/*
-					 * another error!
-					 */
-					showErrorMessage(RETRY_LATER_ERROR_MESSAGE);
+					showErrorMessage(OTHER_ERROR_MESSAGE);
 				}
 
 			}
-			/*
-			 * there is a problem with the server
-			 */
 			else {
-				showErrorMessage(RETRY_LATER_ERROR_MESSAGE);
+				showErrorMessage(SERVER_ERROR_MESSAGE);
 			}
 		} catch (IOException ex) {
 			showErrorMessage(DEFAULT_ERROR_MESSAGE + ex.getMessage());
+		}
+	}
+
+	private int showUpdateToLatestVersionConfirmDialog(String latestversion,
+			String changelog) {
+		return JOptionPane
+				.showConfirmDialog(
+						manager.gui,
+						"A new version of DirBuster ("
+								+ latestversion
+								+ ") is available\n\n"
+								+ "Change log:\n\n"
+								+ changelog
+								+ "\n\n"
+								+ "Do you wish to get the new version now?\n\n"
+								+ "(Auto checking can be disabled from 'Advanced Options -> DirBuster Options')",
+						"A new version of DirBuster is Avaliable",
+						JOptionPane.OK_CANCEL_OPTION);
+	}
+
+	private void showAlreadyLatestVersionMessage() {
+		if (informUser) {
+			JOptionPane.showMessageDialog(manager.gui,
+					"You are running the latest version",
+					"You are running the latest version",
+					JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 
@@ -204,8 +177,7 @@ public class CheckForUpdates implements Runnable {
 		System.out.println("@@@@@@@@@@@@@@@@@@@");
 		System.out.println("Version " + latestversion
 				+ " of DirBuster is available");
-		System.out
-				.println("Download it from: https://sourceforge.net/project/showfiles.php?group_id=199126");
+		System.out.println("Download it from: " + DIRBUSTER_SOURCEFORGE_URL);
 		System.out.println("@@@@@@@@@@@@@@@@@@@");
 	}
 
