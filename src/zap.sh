0d7f7282c0ad62d595677aa284a613a2fa5ac7d0
psiinon <psiinon@gmail.com>
2013-02-04 13:19:50 +0000

    Issue 484: 	Check java version in zap.sh

--- src/zap.sh
+++ src/zap.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+#Check the java version (min java 7)
+JAVAV=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
+
+if [[ $JAVAV == 1.7* ]]; then
+    # OK
+    echo "Using Java version: $JAVAV"
+else
+    echo "Exiting: ZAP 2.0.0 requires a minimum of Java 7 to run."
+    exit 1
+fi
+
 #Dereference from link to the real directory
 SCRIPTNAME=$0
 
 #While name of this script is symbolic link
 while [ -L "$SCRIPTNAME" ] ; do 
-        #Dereference the link to the name of the link target 
-        SCRIPTNAME=$(ls -l "$SCRIPTNAME" | awk '{ print $NF; }')
+    #Dereference the link to the name of the link target 
+    SCRIPTNAME=$(ls -l "$SCRIPTNAME" | awk '{ print $NF; }')
 done
 
 #Base directory where ZAP is installed
