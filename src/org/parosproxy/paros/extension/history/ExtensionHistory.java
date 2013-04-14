89b709140402c3b9fea83f0e1bc28e3dd331b8b0
thc202 <thc202@gmail.com>
2013-04-14 14:14:33 +0000

    Issue 588 - ExtensionHistory.historyIdToRef should be cleared when changing session
    Changed to clear the instance variable ExtensionHistory.historyIdToRef when changing sessions.

--- src/org/parosproxy/paros/extension/history/ExtensionHistory.java
+++ src/org/parosproxy/paros/extension/history/ExtensionHistory.java
@@ -41,6 +41,7 @@
 // ZAP: 2012/10/08 Issue 391: Performance improvements
 // ZAP: 2013/03/03 Issue 546: Remove all template Javadoc comments
 // ZAP: 2013/03/03 Issue 547: Deprecate unused classes and methods
+// ZAP: 2013/04/14 Issue 588: ExtensionHistory.historyIdToRef should be cleared when changing session
 
 package org.parosproxy.paros.extension.history;
 
@@ -216,6 +217,7 @@ public class ExtensionHistory extends ExtensionAdaptor implements SessionChanged
 	
 	private void sessionChangedEventHandler(Session session) {
 	    getHistoryList().clear();
+	    historyIdToRef.clear();
 	    getLogPanel().getListLog().setModel(getHistoryList());
 	    if (getView() != null) { 
 	    	getView().getRequestPanel().clearView(true);
