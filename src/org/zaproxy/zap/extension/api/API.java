3613e277460f3659bf8c0e482cce382b0eb4c6f1
thc202 <thc202@gmail.com>
2013-05-16 23:35:52 +0000

    Issue 666 - JSONException while calling an API action without the required parameter(s)
    Changed to check if the parameter exists (instead of getting it).

--- src/org/zaproxy/zap/extension/api/API.java
+++ src/org/zaproxy/zap/extension/api/API.java
@@ -240,7 +240,7 @@ public class API {
 							List<String> mandatoryParams = action.getMandatoryParamNames();
 							if (mandatoryParams != null) {
 								for (String param : mandatoryParams) {
-									if (params.getString(param) == null || params.getString(param).length() == 0) {
+									if (!params.has(param) || params.getString(param).length() == 0) {
 										throw new ApiException(ApiException.Type.MISSING_PARAMETER, param);
 									}
 								}
