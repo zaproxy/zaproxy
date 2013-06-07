f9f2fdd6e64b51372ca332480998f0ac61df417d
psiinon <psiinon@gmail.com>
2013-06-07 16:47:48 +0000

    Minor  layout fix.

--- src/org/zaproxy/zap/extension/ascan/OptionsScannerPanel.java
+++ src/org/zaproxy/zap/extension/ascan/OptionsScannerPanel.java
@@ -120,7 +120,7 @@ public class OptionsScannerPanel extends AbstractParamPanel {
 			panelScanner.add(getComboStrength(), 
 					LayoutHelper.getGBC(1, 9, 1,  0.0D, 0, GridBagConstraints.HORIZONTAL, new Insets(2,2,2,2)));
 			panelScanner.add(getStrengthNotes(), 
-					LayoutHelper.getGBC(2, 10, 1,  1.0D, 0, GridBagConstraints.HORIZONTAL, new Insets(2,2,2,2)));
+					LayoutHelper.getGBC(2, 9, 1,  1.0D, 0, GridBagConstraints.HORIZONTAL, new Insets(2,2,2,2)));
 			
 			/*
 			panelScanner.add(new JLabel(Constant.messages.getString("ascan.options.params.label")), 
