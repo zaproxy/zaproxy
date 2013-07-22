dadde265d1c495e6137178c24154cba048634e67
thc202 <thc202@gmail.com>
2013-07-22 08:55:55 +0000

    Deleted branch 1.3 (no longer needed).

--- 1.3/src/org/zaproxy/zap/extension/httppanel/HttpPanelHexUi.java
+++
@@ -1,9 +0,0 @@
-package org.zaproxy.zap.extension.httppanel;
-
-import org.parosproxy.paros.extension.AbstractPanel;
-
-public class HttpPanelHexUi extends AbstractPanel {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- 1.3/src/zap.sh
+++
@@ -1,1 +0,0 @@
-java -jar zap.jar org.zaproxy.zap.ZAP $*
