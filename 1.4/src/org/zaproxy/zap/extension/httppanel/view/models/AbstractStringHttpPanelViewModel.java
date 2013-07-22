dd8066861ce4f4f7ec71b5ebe08d7ae599c6a807
thc202 <thc202@gmail.com>
2013-07-22 08:57:06 +0000

    Deleted branch 1.4 (no longer needed).

--- 1.4/src/org/zaproxy/zap/extension/httppanel/view/models/AbstractStringHttpPanelViewModel.java
+++
@@ -1,9 +0,0 @@
-package org.zaproxy.zap.extension.httppanel.view.models;
-
-import org.zaproxy.zap.extension.httppanel.view.DefaultHttpPanelViewModel;
-
-public abstract class AbstractStringHttpPanelViewModel extends DefaultHttpPanelViewModel {
-
-	public abstract String getData();
-	public abstract void setData(String data);
-}
