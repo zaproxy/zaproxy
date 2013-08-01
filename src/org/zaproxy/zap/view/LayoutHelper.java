8e96d6097076dcb1be279c8d46422724cca13363
cosminstefanxp <cosminstefanxp@gmail.com>
2013-08-01 23:11:42 +0000

    Added some utility methods to LayoutHelper to also set height of Constraints.

--- src/org/zaproxy/zap/view/LayoutHelper.java
+++ src/org/zaproxy/zap/view/LayoutHelper.java
@@ -32,11 +32,19 @@ public class LayoutHelper {
 	}
 	
 	public static GridBagConstraints getGBC(int x, int y, int width, double weightx, Insets insets) {
-		return getGBC(x, y, width, weightx, 0.0, DEFAULT_FILL, DEFAULT_ANCHOR, insets);
+		return getGBC(x, y, width, 1, weightx, 0.0, DEFAULT_FILL, DEFAULT_ANCHOR, insets);
+	}
+	
+	public static GridBagConstraints getGBC(int x, int y, int width, int height, double weightx, Insets insets) {
+		return getGBC(x, y, width, height, weightx, 0.0, DEFAULT_FILL, DEFAULT_ANCHOR, insets);
 	}
 	
 	public static GridBagConstraints getGBC(int x, int y, int width, double weightx, double weighty) {
 		return getGBC(x, y, width, weightx, weighty, DEFAULT_FILL, null);
+	}
+	
+	public static GridBagConstraints getGBC(int x, int y, int width, int height, double weightx, double weighty) {
+		return getGBC(x, y, width, height, weightx, weighty, DEFAULT_FILL, DEFAULT_ANCHOR, null);
 	}
 	
 	public static GridBagConstraints getGBC(int x, int y, int width, double weightx, double weighty, int fill) {
@@ -45,10 +53,10 @@ public class LayoutHelper {
 	
 	public static GridBagConstraints getGBC(int x, int y, int width, double weightx, double weighty, int fill, 
 			Insets insets) {
-		return getGBC(x, y, width, weightx, weighty, fill, DEFAULT_ANCHOR, insets);
+		return getGBC(x, y, width, 1, weightx, weighty, fill, DEFAULT_ANCHOR, insets);
 	}
 	
-	public static GridBagConstraints getGBC(int x, int y, int width, double weightx, double weighty, int fill, int anchor, 
+	public static GridBagConstraints getGBC(int x, int y, int width, int height, double weightx, double weighty, int fill, int anchor, 
 			Insets insets) {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = x;
@@ -61,6 +69,7 @@ public class LayoutHelper {
 		gbc.weightx = weightx;
 		gbc.weighty = weighty;
 		gbc.gridwidth = width;
+		gbc.gridheight = height;
 		return gbc;
 	}
 
