a4d2db2c3925075f1f0ad4aba67f5077c325359c
cosminstefanxp <cosminstefanxp@gmail.com>
2013-09-23 18:17:52 +0000

    Added API call for spidering an URL from the point of view of an User

--- src/org/zaproxy/zap/extension/spider/SpiderAPI.java
+++ src/org/zaproxy/zap/extension/spider/SpiderAPI.java
@@ -26,20 +26,26 @@ import net.sf.json.JSONObject;
 import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
 import org.apache.log4j.Logger;
+import org.parosproxy.paros.control.Control;
 import org.parosproxy.paros.model.Model;
 import org.parosproxy.paros.model.Session;
 import org.parosproxy.paros.model.SiteNode;
 import org.parosproxy.paros.network.HttpMessage;
 import org.zaproxy.zap.extension.api.ApiAction;
 import org.zaproxy.zap.extension.api.ApiException;
+import org.zaproxy.zap.extension.api.ApiException.Type;
 import org.zaproxy.zap.extension.api.ApiImplementor;
 import org.zaproxy.zap.extension.api.ApiResponse;
 import org.zaproxy.zap.extension.api.ApiResponseElement;
 import org.zaproxy.zap.extension.api.ApiResponseList;
 import org.zaproxy.zap.extension.api.ApiView;
+import org.zaproxy.zap.extension.users.ExtensionUserManagement;
+import org.zaproxy.zap.model.Context;
 import org.zaproxy.zap.model.ScanListenner;
 import org.zaproxy.zap.spider.SpiderListener;
 import org.zaproxy.zap.spider.filters.FetchFilter.FetchStatus;
+import org.zaproxy.zap.users.User;
+import org.zaproxy.zap.utils.ApiUtils;
 
 public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderListener {
 
@@ -49,6 +55,7 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 	private static final String PREFIX = "spider";
 	/** The Constant ACTION_START_SCAN that defines the action of starting a new scan. */
 	private static final String ACTION_START_SCAN = "scan";
+	private static final String ACTION_START_SCAN_AS_USER = "scanAsUser";
 
 	/** The Constant ACTION_STOP_SCAN that defines the action of stopping a pending scan. */
 	private static final String ACTION_STOP_SCAN = "stop";
@@ -69,6 +76,8 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 	 * The Constant PARAM_URL that defines the parameter defining the url of the scan.
 	 */
 	private static final String PARAM_URL = "url";
+	private static final String PARAM_USER_ID = "userId";
+	private static final String PARAM_CONTEXT_ID = "contextId";
 	private static final String PARAM_REGEX = "regex";
 
 	private static final String ACTION_EXCLUDE_FROM_SCAN = "excludeFromScan";
@@ -98,6 +107,8 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 		this.foundURIs = new ArrayList<>();
 		// Register the actions
 		this.addApiAction(new ApiAction(ACTION_START_SCAN, new String[] { PARAM_URL }));
+		this.addApiAction(new ApiAction(ACTION_START_SCAN_AS_USER, new String[] { PARAM_URL,
+				PARAM_CONTEXT_ID, PARAM_USER_ID }));
 		this.addApiAction(new ApiAction(ACTION_STOP_SCAN));
 		this.addApiAction(new ApiAction(ACTION_CLEAR_EXCLUDED_FROM_SCAN));
 		this.addApiAction(new ApiAction(ACTION_EXCLUDE_FROM_SCAN, new String[] { PARAM_REGEX }));
@@ -118,29 +129,44 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 	public ApiResponse handleApiAction(String name, JSONObject params) throws ApiException {
 		log.debug("Request for handleApiAction: " + name + " (params: " + params.toString() + ")");
 
-		if (ACTION_START_SCAN.equals(name)) {
+		switch (name) {
+		case ACTION_START_SCAN:
 			// The action is to start a new Scan
-			String url = params.getString(PARAM_URL);
+			String url = ApiUtils.getNonEmptyStringParam(params, PARAM_URL);
+			scanURL(url, null);
+			break;
+		case ACTION_START_SCAN_AS_USER:
+			// The action is to start a new Scan from the perspective of a user
+			String urlUserScan = ApiUtils.getNonEmptyStringParam(params, PARAM_URL);
+			int userID = ApiUtils.getIntParam(params, PARAM_USER_ID);
+			ExtensionUserManagement usersExtension = (ExtensionUserManagement) Control.getSingleton()
+					.getExtensionLoader().getExtension(ExtensionUserManagement.NAME);
+			if (usersExtension == null)
+				throw new ApiException(Type.NO_IMPLEMENTOR, ExtensionUserManagement.NAME);
+			Context context = ApiUtils.getContextByParamId(params, PARAM_CONTEXT_ID);
+			if (!context.isIncluded(urlUserScan))
+				throw new ApiException(Type.URL_NOT_IN_CONTEXT, PARAM_CONTEXT_ID);
+			User user = usersExtension.getContextUserAuthManager(context.getIndex()).getUserById(userID);
+			if (user == null)
+				throw new ApiException(Type.USER_NOT_FOUND, PARAM_USER_ID);
+			scanURL(urlUserScan, user);
+			break;
 
-			// Check for required parameter
-			if (url == null || url.length() == 0) {
-				throw new ApiException(ApiException.Type.MISSING_PARAMETER, PARAM_URL);
-			}
-
-			scanURL(url);
-		} else if (ACTION_STOP_SCAN.equals(name)) {
+		case ACTION_STOP_SCAN:
 			// The action is to stop a pending scan
 			if (spiderThread != null) {
 				spiderThread.stopScan();
 			}
-		} else if (ACTION_CLEAR_EXCLUDED_FROM_SCAN.equals(name)) {
+			break;
+		case ACTION_CLEAR_EXCLUDED_FROM_SCAN:
 			try {
 				Session session = Model.getSingleton().getSession();
 				session.setExcludeFromSpiderRegexs(new ArrayList<String>());
 			} catch (SQLException e) {
 				throw new ApiException(ApiException.Type.INTERNAL_ERROR, e.getMessage());
 			}
-		} else if (ACTION_EXCLUDE_FROM_SCAN.equals(name)) {
+			break;
+		case ACTION_EXCLUDE_FROM_SCAN:
 			String regex = params.getString(PARAM_REGEX);
 			try {
 				Session session = Model.getSingleton().getSession();
@@ -148,7 +174,8 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 			} catch (Exception e) {
 				throw new ApiException(ApiException.Type.BAD_FORMAT, PARAM_REGEX);
 			}
-		} else {
+			break;
+		default:
 			throw new ApiException(ApiException.Type.BAD_ACTION);
 		}
 		return ApiResponseElement.OK;
@@ -161,10 +188,12 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 	/**
 	 * Start the scan of an url.
 	 * 
-	 * @param url the url
+	 * @param url the url to scan
+	 * @param scanUser the user to scan as, or null if the scan is done without the perspective of
+	 *            any user
 	 * @throws ApiException the api exception
 	 */
-	private void scanURL(String url) throws ApiException {
+	private void scanURL(String url, User scanUser) throws ApiException {
 		log.debug("API Spider scanning url: " + url);
 		if (scanInProgress()) {
 			throw new ApiException(ApiException.Type.SCAN_IN_PROGRESS);
@@ -186,13 +215,16 @@ public class SpiderAPI extends ApiImplementor implements ScanListenner, SpiderLi
 			} else {
 				spiderThread.setStartURI(startURI);
 			}
-		
+
+			if (scanUser != null)
+				spiderThread.setScanAsUser(scanUser);
+
 			// Start the scan
 			this.foundURIs.clear();
 			this.progress = 0;
 			spiderThread.addSpiderListener(this);
 			spiderThread.start();
-		
+
 		} catch (URIException e) {
 			throw new ApiException(ApiException.Type.BAD_FORMAT);
 		}
