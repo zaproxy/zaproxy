90bc0f3ea657891577619bc6366bdc93e02f540a
thc202 <thc202@gmail.com>
2013-11-17 02:23:17 +0000

    Issue 901 - Pop up menu "succeed" separator is not added when using sub-menu in MainPopupMenu
    Changed to add a separator instead of the menu item.

--- src/org/parosproxy/paros/view/MainPopupMenu.java
+++ src/org/parosproxy/paros/view/MainPopupMenu.java
@@ -32,6 +32,7 @@
 // ZAP: 2013/04/14 Issue 592: Do not show the main pop up menu if it doesn't have visible pop up menu items
 // ZAP: 2013/04/14 Issue 598: Replace/update "old" pop up menu items
 // ZAP: 2013/11/16 Issue 878: ExtensionPopupMenuItem#getMenuIndex() as no effect in MainPopupMenu
+// ZAP: 2013/11/16 Issue 901: Pop up menu "succeed" separator is not added when using sub-menu in MainPopupMenu
 
 package org.parosproxy.paros.view;
 
@@ -163,7 +164,7 @@ public class MainPopupMenu extends JPopupMenu {
 	            			superMenu.setEnabled(true);
 	            		}
 	            		if (menuItem.succeedWithSeparator()) {
-	            			superMenu.add(menuItem);
+	            			superMenu.addSeparator();
 	            		}
 	            		
 	            	} else {
