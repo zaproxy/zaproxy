55ba352db962509abea56e2d3cb7d691b3196dba
psiinon <psiinon@gmail.com>
2014-03-24 09:17:53 +0000

    Regenerating 2.3 branch from the trunk

--- 2.3/src/org/zaproxy/zap/view/ContextPanelFactory.java
+++
@@ -1,9 +0,0 @@
-package org.zaproxy.zap.view;
-
-import org.zaproxy.zap.model.Context;
-
-public interface ContextPanelFactory {
-	AbstractContextPropertiesPanel getContextPanel(Context ctx);
-	
-	void discardContexts();
-}
