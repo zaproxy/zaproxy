02277b601c86edd4efdbfbdf1436438c5ebd9190
psiinon <psiinon@gmail.com>
2014-05-21 09:13:30 +0000

    Updated the CFU url for 2.3.1

--- src/org/zaproxy/zap/extension/autoupdate/ExtensionAutoUpdate.java
+++ src/org/zaproxy/zap/extension/autoupdate/ExtensionAutoUpdate.java
@@ -64,8 +64,8 @@ import org.zaproxy.zap.view.ZapMenuItem;
 
 public class ExtensionAutoUpdate extends ExtensionAdaptor implements CheckForUpdateCallback {
 	
-	// The short URL means that the number of checkForUpdates can be tracked - see http://goo.gl/info/VjEJBR
-	private static final String ZAP_VERSIONS_XML_SHORT = "http://goo.gl/VjEJBR";
+	// The short URL means that the number of checkForUpdates can be tracked - see http://goo.gl/info/X7b1uT
+	private static final String ZAP_VERSIONS_XML_SHORT = "http://goo.gl/X7b1uT";
     
     private static final String ZAP_VERSIONS_XML_FULL = "http://zaproxy.googlecode.com/svn/wiki/ZapVersions.xml";
 
