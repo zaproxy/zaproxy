193b2a0a037d945d06e7eb009bfb4a3e3eed0944
cosminstefanxp <cosminstefanxp@gmail.com>
2014-06-03 16:01:07 +0000

    Added proper equals and hashCode for Context that compares based on context id.

--- src/org/zaproxy/zap/model/Context.java
+++ src/org/zaproxy/zap/model/Context.java
@@ -511,4 +511,28 @@ public class Context {
 		return newContext;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + index;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Context other = (Context) obj;
+		if (index != other.index)
+			return false;
+		return true;
+	}
+	
+	
+
 }
