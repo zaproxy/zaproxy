f8c4474c952a9efd85f8ba819acb18c96640f74d
thc202 <thc202@gmail.com>
2014-08-14 13:22:20 +0000

    Issue 1303 - HttpPanel[SyntaxHighlight]TextArea leak through HighlighterManager
    Changed HighlighterManager to not hard reference the Observers (instead of adding a (long) call chain of cleanup methods) so they can be GC'ed when no longer needed.

--- src/org/zaproxy/zap/view/HighlighterManager.java
+++ src/org/zaproxy/zap/view/HighlighterManager.java
@@ -1,8 +1,13 @@
 package org.zaproxy.zap.view;
 
 import java.awt.Color;
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Observable;
+import java.util.Observer;
 
 import org.zaproxy.zap.extension.search.SearchMatch;
 
@@ -23,8 +28,11 @@ public class HighlighterManager extends Observable {
 	
 	private LinkedList<HighlightSearchEntry> highlights;
 	
+	private List<SoftObserver> observers;
+	
 	public HighlighterManager() {
 		highlights = new LinkedList<>();
+		observers =  new ArrayList<>();
 	}
 	
 	public void reinitHighlights(LinkedList<HighlightSearchEntry> list) {
@@ -57,5 +65,88 @@ public class HighlighterManager extends Observable {
 	// TODO: sux
 	public LinkedList<HighlightSearchEntry> getHighlights() {
 		return (LinkedList<HighlightSearchEntry>) highlights.clone();
+	}
+
+	@Override
+	public synchronized void addObserver(Observer obs) {
+		if (obs == null) {
+			throw new NullPointerException();
+		}
+
+		boolean observerContained = false;
+		for (Iterator<SoftObserver> it = observers.iterator(); it.hasNext();) {
+			Observer other = it.next().get();
+			if (other == null) {
+				it.remove();
+			} else if (obs.equals(other)) {
+				observerContained = true;
+				break;
+			}
+		}
+		if (!observerContained) {
+			observers.add(new SoftObserver(obs));
+		}
+	}
+
+	@Override
+	public synchronized void deleteObserver(Observer obs) {
+		for (Iterator<SoftObserver> it = observers.iterator(); it.hasNext();) {
+			Observer other = it.next().get();
+			if (other == null) {
+				it.remove();
+			} else if (obs.equals(other)) {
+				it.remove();
+				break;
+			}
+		}
+	}
+
+	private synchronized void deleteSoftObserver(SoftObserver obs) {
+		observers.remove(obs);
+	}
+
+	@Override
+	public void notifyObservers(Object arg) {
+		Observer[] arrLocal;
+
+		synchronized (this) {
+			if (!hasChanged()) {
+				return;
+			}
+			arrLocal = new Observer[observers.size()];
+			arrLocal = observers.toArray(arrLocal);
+			clearChanged();
+		}
+
+		for (int i = arrLocal.length - 1; i >= 0; i--) {
+			arrLocal[i].update(this, arg);
+		}
+	}
+
+	@Override
+	public synchronized void deleteObservers() {
+		observers.clear();
+	}
+
+	@Override
+	public synchronized int countObservers() {
+		return observers.size();
+	}
+
+	private class SoftObserver extends SoftReference<Observer> implements Observer {
+
+		public SoftObserver(Observer referent) {
+			super(referent);
+		}
+
+		@Override
+		public void update(Observable o, Object arg) {
+			Observer observer = get();
+			if (observer != null) {
+				observer.update(o, arg);
+			} else {
+				deleteSoftObserver(this);
+			}
+		}
 	}
 }
