d64a53704ea6a0d002863794ce1e3219d1eb2185
cosminstefanxp <cosminstefanxp@gmail.com>
2014-08-18 08:52:41 +0000

    Added some utility methods to ReportGenerator and an access control report xsl

--- src/org/parosproxy/paros/extension/report/ReportGenerator.java
+++ src/org/parosproxy/paros/extension/report/ReportGenerator.java
@@ -15,7 +15,7 @@ Clarified Artistic License for more details.
 You should have received a copy of the Clarified Artistic License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ */
 // ZAP: 2011/11/04 Correct entityEncode
 // ZAP: 2011/12/19 Escape invalid XML characters
 // ZAP: 2012/08/07 synchronize calls on staticDateFormat
@@ -34,6 +34,7 @@ import java.util.Date;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -44,6 +45,7 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.zaproxy.zap.utils.XMLStringUtil;
@@ -52,152 +54,186 @@ public class ReportGenerator {
 
 	private static final Logger logger = Logger.getLogger(ReportGenerator.class);
 
-//    private static Pattern patternWindows = Pattern.compile("window", Pattern.CASE_INSENSITIVE);
-//	private static Pattern patternLinux = Pattern.compile("linux", Pattern.CASE_INSENSITIVE);
+	// private static Pattern patternWindows = Pattern.compile("window", Pattern.CASE_INSENSITIVE);
+	// private static Pattern patternLinux = Pattern.compile("linux", Pattern.CASE_INSENSITIVE);
 
 	private static final SimpleDateFormat staticDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
-	
-	public static File stringToHtml (String inxml, String infilexsl, String outfilename) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    	Document doc = null;
 
-        //factory.setNamespaceAware(true);
-        //factory.setValidating(true);
- 		File stylesheet 		= null;
- 		File outfile			= null;
- 		StringReader inReader	= new StringReader(inxml);
- 		
-        try {
-            stylesheet = new File(infilexsl);
-            outfile	= new File(outfilename);
- 
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            doc = builder.parse(new InputSource(inReader));
-            
-            // Use a Transformer for output
-            TransformerFactory tFactory =
-                TransformerFactory.newInstance();
-            StreamSource stylesource = new StreamSource(stylesheet);
-            Transformer transformer = tFactory.newTransformer(stylesource);
- 
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(outfile);
-            transformer.transform(source, result);
+	public static File XMLToHtml(Document xmlDocument, String infilexsl, File outFile) {
+		File stylesheet = null;
 
-        } catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
+		outFile = new File(outFile.getAbsolutePath());
+		try {
+			stylesheet = new File(infilexsl);
 
-        }
-                
+			DOMSource source = new DOMSource(xmlDocument);
+
+			// Use a Transformer for output
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			StreamSource stylesource = new StreamSource(stylesheet);
+			Transformer transformer = tFactory.newTransformer(stylesource);
+
+			// Make the transformation and write to the output file
+			StreamResult result = new StreamResult(outFile);
+			transformer.transform(source, result);
+
+		} catch (TransformerException e) {
+			logger.error(e.getMessage(), e);
+		}
+
+		return outFile;
+	}
+
+	public static File stringToHtml(String inxml, String infilexsl, String outfilename) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document doc = null;
+
+		// factory.setNamespaceAware(true);
+		// factory.setValidating(true);
+		File stylesheet = null;
+		File outfile = null;
+		StringReader inReader = new StringReader(inxml);
+
+		try {
+			stylesheet = new File(infilexsl);
+			outfile = new File(outfilename);
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			doc = builder.parse(new InputSource(inReader));
+
+			// Use a Transformer for output
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			StreamSource stylesource = new StreamSource(stylesheet);
+			Transformer transformer = tFactory.newTransformer(stylesource);
+
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(outfile);
+			transformer.transform(source, result);
+
+		} catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+
+		}
+
 		return outfile;
+	}
 
-    }
+	public static String stringToHtml(String inxml, String infilexsl) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document doc = null;
 
-	public static String stringToHtml (String inxml, String infilexsl) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    	Document doc = null;
+		// factory.setNamespaceAware(true);
+		// factory.setValidating(true);
+		File stylesheet = null;
+		StringReader inReader = new StringReader(inxml);
+		StringWriter writer = new StringWriter();
 
-        //factory.setNamespaceAware(true);
-        //factory.setValidating(true);
- 		File stylesheet 		= null;
- 		StringReader inReader	= new StringReader(inxml);
- 		StringWriter writer = new StringWriter();
- 		
-        try {
-            stylesheet = new File(infilexsl);
- 
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            doc = builder.parse(new InputSource(inReader));
-            
-            // Use a Transformer for output
-            TransformerFactory tFactory =
-                TransformerFactory.newInstance();
-            StreamSource stylesource = new StreamSource(stylesheet);
-            Transformer transformer = tFactory.newTransformer(stylesource);
- 
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(writer);
-            transformer.transform(source, result);
+		try {
+			stylesheet = new File(infilexsl);
 
-        } catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			doc = builder.parse(new InputSource(inReader));
 
-        }
-                
+			// Use a Transformer for output
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			StreamSource stylesource = new StreamSource(stylesheet);
+			Transformer transformer = tFactory.newTransformer(stylesource);
+
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(writer);
+			transformer.transform(source, result);
+
+		} catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+
+		}
+
 		return writer.toString();
 
-    }
+	}
 
-    
-    public static File fileToHtml (String infilexml, String infilexsl, String outfilename) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    	Document doc = null;
+	public static File fileToHtml(String infilexml, String infilexsl, String outfilename) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document doc = null;
 
-        //factory.setNamespaceAware(true);
-        //factory.setValidating(true);
- 		File stylesheet 	= null;
- 		File datafile		= null;
- 		File outfile		= null;
- 		
-        try {
-            stylesheet = new File(infilexsl);
-            datafile   = new File(infilexml);
-            outfile	= new File(outfilename);
- 
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            doc = builder.parse(datafile);
- 
-            // Use a Transformer for output
-            TransformerFactory tFactory =
-                TransformerFactory.newInstance();
-            StreamSource stylesource = new StreamSource(stylesheet);
-            Transformer transformer = tFactory.newTransformer(stylesource);
- 
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(outfile);
-            transformer.transform(source, result);
+		// factory.setNamespaceAware(true);
+		// factory.setValidating(true);
+		File stylesheet = null;
+		File datafile = null;
+		File outfile = null;
 
-        } catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
+		try {
+			stylesheet = new File(infilexsl);
+			datafile = new File(infilexml);
+			outfile = new File(outfilename);
 
-        }
-                
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			doc = builder.parse(datafile);
+
+			// Use a Transformer for output
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			StreamSource stylesource = new StreamSource(stylesheet);
+			Transformer transformer = tFactory.newTransformer(stylesource);
+
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(outfile);
+			transformer.transform(source, result);
+
+		} catch (TransformerException | SAXException | ParserConfigurationException | IOException e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+
+		}
+
 		return outfile;
 
-    }
-    
+	}
+
 	/**
-	Encode entity for HTML or XML output.
-	*/
+	 * Encode entity for HTML or XML output.
+	 */
 	public static String entityEncode(String text) {
 		String result = text;
-		
+
 		if (result == null) {
 			return result;
 		}
 
 		// The escapeXml function doesnt cope with some 'special' chrs
-		
+
 		return StringEscapeUtils.escapeXml(XMLStringUtil.escapeControlChrs(result));
 	}
-	
+
 	/**
-	Get today's date string.
-	*/
+	 * Get today's date string.
+	 */
 	public static String getCurrentDateTimeString() {
 		Date dateTime = new Date(System.currentTimeMillis());
 		return getDateTimeString(dateTime);
-		
+
 	}
 
 	public static String getDateTimeString(Date dateTime) {
 		// ZAP: fix unsafe call to DateFormats
 		synchronized (staticDateFormat) {
-		    return staticDateFormat.format(dateTime);
+			return staticDateFormat.format(dateTime);
 		}
 	}
-	
+
+	public static void addChildTextNode(Document doc, Element parent, String nodeName, String text) {
+		Element child = doc.createElement(nodeName);
+		child.appendChild(doc.createTextNode(text));
+		parent.appendChild(child);
+	}
+
+	public static String getDebugXMLString(Document doc) throws TransformerException {
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		return writer.getBuffer().toString().replaceAll("\n|\r", "");
+	}
 }
