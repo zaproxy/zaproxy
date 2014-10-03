b8fca631b4aa328f6d094f68edc70046d3fdf70e
psiinon <psiinon@gmail.com>
2014-10-03 13:38:51 +0000

    Issue 1352: Relicensed the files imported from WebScarab with permission from Rogan Dawes

--- src/ch/csnc/extension/httpclient/SSLContextManager.java
+++ src/ch/csnc/extension/httpclient/SSLContextManager.java
@@ -4,19 +4,25 @@
  *
  * Copyright (c) 2002 - 2004 Rogan Dawes
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Please note that this file was originally released under the 
+ * GNU General Public License  as published by the Free Software Foundation; 
+ * either version 2 of the License, or (at your option) any later version.
+ * 
+ * As of October 2014 Rogan Dawes granted the OWASP ZAP Project permission to 
+ * redistribute this code under the Apache License, Version 2.0: 
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
  */
 
 package ch.csnc.extension.httpclient;
