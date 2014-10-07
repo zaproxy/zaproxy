dbd1872b7d3baed8cd69e91b3df443e730574503
psiinon <psiinon@gmail.com>
2014-10-07 20:15:18 +0000

    Issue 1352: Relicensed the files originally copyrighted by Compass Security AG and Axel Neumann with permission from Compass Security AG and Axel Neumann

--- src/ch/csnc/extension/ui/AliasTableModel.java
+++ src/ch/csnc/extension/ui/AliasTableModel.java
@@ -1,19 +1,29 @@
 /*
- * Copyright (C) 2010, Compass Security AG
+ * Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright the ZAP Development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Please note that this file was originally released under the 
+ * GNU General Public License  as published by the Free Software Foundation; 
+ * either version 2 of the License, or (at your option) any later version
+ * by Compass Security AG
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/copyleft/
- * 
+ * As of October 2014 Compass Security AG granted the OWASP ZAP Project 
+ * permission to redistribute this code under the Apache License, Version 2.0 
  */
 
 package ch.csnc.extension.ui;
