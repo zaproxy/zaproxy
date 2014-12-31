624fc0ad10ccd194e363f52139fbc0a3a2ca9854
psiinon <psiinon@gmail.com>
2014-12-31 10:18:46 +0000

    Added alert event publisher (copied from trunk)

---
+++ src/org/zaproxy/zap/extension/alert/AlertEventPublisher.java
@@ -0,0 +1,47 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Copyright The ZAP development team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ */
+
+package org.zaproxy.zap.extension.alert;
+
+import org.zaproxy.zap.ZAP;
+import org.zaproxy.zap.eventBus.EventPublisher;
+
+public class AlertEventPublisher implements EventPublisher {
+	
+	private static AlertEventPublisher publisher = null;
+	public static final String ALERT_ADDED_EVENT	= "alert.added";
+	public static final String ALERT_REMOVED_EVENT	= "alert.removed";
+	public static final String ALL_ALERTS_REMOVED_EVENT	= "alert.all.removed";
+	
+	@Override
+	public String getPublisherName() {
+		return AlertEventPublisher.class.getCanonicalName();
+	}
+
+	public static synchronized AlertEventPublisher getPublisher() {
+		if (publisher == null) {
+			publisher = new AlertEventPublisher(); 
+	        ZAP.getEventBus().registerPublisher(publisher, 
+	        		new String[] {ALERT_ADDED_EVENT, ALERT_REMOVED_EVENT, ALL_ALERTS_REMOVED_EVENT});
+
+		}
+		return publisher;
+	}
+}
