f72bec9b2aad59176d089bad829aec19b1d531f4
thc202 <thc202@gmail.com>
2015-01-05 02:40:26 +0000

    Issue 1475 - Alerts with different name from same scanner might not be shown in report
    Changed the method Alert.equals(Object) to also check the name of the alert.

--- src/org/parosproxy/paros/core/scanner/Alert.java
+++ src/org/parosproxy/paros/core/scanner/Alert.java
@@ -36,6 +36,7 @@
 // ZAP: 2014/04/10 Issue 1042: Having significant issues opening a previous session
 // ZAP: 2014/05/23 Issue 1209: Reliability becomes Confidence and add levels
 // ZAP: 2015/01/04 Issue 1419: Include alert's evidence in HTML report
+// ZAP: 2014/01/04 Issue 1475: Alerts with different name from same scanner might not be shown in report
 
 package org.parosproxy.paros.core.scanner;
 
@@ -322,14 +323,14 @@ public class Alert implements Comparable<Object>  {
 
 
 	/**
-	Override equals.  Alerts are equal if the alert, uri and param is the same.
+	Override equals.  Alerts are equal if the plugin id, alert, other info, uri and param is the same.
 	*/
 	@Override
 	public boolean equals(Object obj) {
 		Alert item = null;
 		if (obj instanceof Alert) {
 			item = (Alert) obj;
-			if ((pluginId == item.pluginId) && uri.equalsIgnoreCase(item.uri)
+			if ((pluginId == item.pluginId) && alert.equals(item.alert) && uri.equalsIgnoreCase(item.uri)
 				&& param.equalsIgnoreCase(item.param) && otherInfo.equalsIgnoreCase(item.otherInfo)) {
 				return true;
 			}
