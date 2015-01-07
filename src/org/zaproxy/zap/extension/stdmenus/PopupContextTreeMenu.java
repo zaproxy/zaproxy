7d5edda8c0c9d977418f27ce76b43661a0d03d70
psiinon <psiinon@gmail.com>
2015-01-07 15:40:58 +0000

    Fixed NPE when right clicking in Context tree with no node selected

--- src/org/zaproxy/zap/extension/stdmenus/PopupContextTreeMenu.java
+++ src/org/zaproxy/zap/extension/stdmenus/PopupContextTreeMenu.java
@@ -47,7 +47,7 @@ public class PopupContextTreeMenu extends ExtensionPopupMenuItem {
 		if (invoker instanceof JTree && SiteMapPanel.CONTEXT_TREE_COMPONENT_NAME.equals(invoker.getName())) {
 			JTree contextTree = (JTree)invoker;
 			SiteNode node = (SiteNode)contextTree.getLastSelectedPathComponent();
-			if (node.isRoot()) {
+			if (node == null || node.isRoot()) {
 				return false;
 			}
 			contextId = ((Target)node.getUserObject()).getContext().getIndex();
