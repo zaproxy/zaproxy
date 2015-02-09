5dc55280e18932f6b6633c4522cd56e8d30e61b0
thc202 <thc202@gmail.com>
2015-02-09 09:09:58 +0000

    Use the latest options set when in advanced active scan
    Since it's not possible to configure the thread count, concurrent hosts,
    handle anti CSRF tokens and max results to list when configuring
    an advanced active scan it needs to be updated with the latest value set
    in the options otherwise it would use previous (outdated) values.

--- src/org/zaproxy/zap/extension/ascan/CustomScanDialog.java
+++ src/org/zaproxy/zap/extension/ascan/CustomScanDialog.java
@@ -912,6 +912,11 @@ public class CustomScanDialog extends StandardFieldsDialog {
                 }
             }
 
+            scannerParam.setHostPerScan(extension.getScannerParam().getHostPerScan());
+            scannerParam.setThreadPerHost(extension.getScannerParam().getThreadPerHost());
+            scannerParam.setHandleAntiCSRFTokens(extension.getScannerParam().getHandleAntiCSRFTokens());
+            scannerParam.setMaxResultsToList(extension.getScannerParam().getMaxResultsToList());
+
             contextSpecificObjects = new Object[]{
                 scannerParam,
                 scanPolicy,
