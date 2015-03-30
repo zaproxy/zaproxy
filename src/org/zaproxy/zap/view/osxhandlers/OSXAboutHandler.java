acf72e20947d12780bd9569b23559363a3019c71
marumari <soylentmean@gmail.com>
2015-03-30 17:13:34 +0000

    Committing the non-build related changes from the last week or two.

---
+++ src/org/zaproxy/zap/view/osxhandlers/OSXAboutHandler.java
@@ -0,0 +1,14 @@
+package org.zaproxy.zap.view.osxhandlers;
+
+import org.parosproxy.paros.view.View;
+import org.zaproxy.zap.view.AboutDialog;
+
+import com.apple.eawt.AboutHandler;
+import com.apple.eawt.AppEvent.AboutEvent;
+
+public class OSXAboutHandler implements AboutHandler {
+    public void handleAbout(AboutEvent ae) {
+        AboutDialog dialog = new AboutDialog(View.getSingleton().getMainFrame(), true);
+        dialog.setVisible(true);
+    }
+}
