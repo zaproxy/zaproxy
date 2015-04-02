7cdb5b17d4c844d7224c24183452e602707a58ba
psiinon <psiinon@gmail.com>
2015-04-02 16:05:35 +0000

    Added missing headers and removed debug code

--- src/org/zaproxy/zap/model/StructuralTableNodeIterator.java
+++ src/org/zaproxy/zap/model/StructuralTableNodeIterator.java
@@ -1,3 +1,22 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Copyright The ZAP Development Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
 package org.zaproxy.zap.model;
 
 import java.util.Iterator;
@@ -10,6 +29,7 @@ import org.parosproxy.paros.model.Model;
 
 public class StructuralTableNodeIterator implements Iterator<StructuralNode> {
 
+	@SuppressWarnings("unused")
 	private StructuralNode parent;
 	private List<RecordStructure> children;
 	private int index = 0;
@@ -18,16 +38,10 @@ public class StructuralTableNodeIterator implements Iterator<StructuralNode> {
 		this.parent = parent;
 		// TODO handle v large numbers of children?
 		try {
-			System.out.println("SBSB StructuralTableNodeIterator for parent: " + parent);
-			System.out.println("SBSB StructuralTableNodeIterator for parent name: " + parent.getName());
-			
 			children = Model.getSingleton().getDb().getTableStructure().getChildren(
 					parent.getRecordStructure().getSessionId(), parent.getRecordStructure().getStructureId());
-			System.out.println("SBSB StructuralTableNodeIterator children: " + children);
-			System.out.println("SBSB StructuralTableNodeIterator children size: " + children.size());
 		} catch (DatabaseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Ignore - if there are db problems then load of errors will get logged
 		}
 	}
 	
@@ -48,7 +62,7 @@ public class StructuralTableNodeIterator implements Iterator<StructuralNode> {
 
 	@Override
 	public void remove() {
-		// TODO Auto-generated method stub
+		// TODO 
 		
 	}
 
