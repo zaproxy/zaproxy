689c1eda5e9f0a4613a62b14e12e8466f24cb3d0
thc202 <thc202@gmail.com>
2015-04-09 07:51:09 +0000

    ExtensionAutoUpdate: Fix NullPointerException after installing an add-on
    If an add-on, not available in marketplace, is installed locally after
    checking for updates a NPE was thrown as no check was being made to verify
    that the add-on really existed in the collection of available add-ons.

--- src/org/zaproxy/zap/extension/autoupdate/ExtensionAutoUpdate.java
+++ src/org/zaproxy/zap/extension/autoupdate/ExtensionAutoUpdate.java
@@ -789,7 +789,7 @@ public class ExtensionAutoUpdate extends ExtensionAdaptor implements CheckForUpd
 
         if (latestVersionInfo != null) {
             AddOn addOn = latestVersionInfo.getAddOn(ao.getId());
-            if (AddOn.InstallationStatus.DOWNLOADING == addOn.getInstallationStatus()) {
+            if (addOn != null && AddOn.InstallationStatus.DOWNLOADING == addOn.getInstallationStatus()) {
                 addOn.setInstallationStatus(AddOn.InstallationStatus.INSTALLED);
             }
         }
