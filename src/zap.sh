77b751b0ca8add2c75372059952bd5889e70ab52
psiinon <psiinon@gmail.com>
2015-04-15 08:18:13 +0000

    Issue 1598: startup script lacks support for FreeBSD

--- src/zap.sh
+++ src/zap.sh
@@ -48,6 +48,10 @@ if [ "$OS" = "Linux" ]; then
 	MEM=$(expr $(sed -n 's/MemTotal:[ ]\{1,\}\([0-9]\{1,\}\) kB/\1/p' /proc/meminfo) / 1024)
 elif [ "$OS" = "Darwin" ]; then
 	MEM=$(system_profiler SPMemoryDataType | sed -n -e 's/.*Size: \([0-9]\{1,\}\) GB/\1/p' | awk '{s+=$0} END {print s*1024}')
+elif [ "$OS" = "SunOS" ]; then
+	MEM=$(/usr/sbin/prtconf | awk '/Memory/{print $3}')
+elif [ "$OS" = "FreeBSD" ]; then
+	MEM=$(($(sysctl -n hw.physmem)/1024/1024))
 fi
 
 if [ -z $MEM ]
