11a815f744425559a3cb57c424e532def36a30f0
psiinon <psiinon@gmail.com>
2015-04-15 08:56:39 +0000

    Updated java client API for 2.4.0

---
+++ src/org/zaproxy/clientapi/gen/Selenium.java
@@ -0,0 +1,104 @@
+/* Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright the ZAP development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.zaproxy.clientapi.gen;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.zaproxy.clientapi.core.ApiResponse;
+import org.zaproxy.clientapi.core.ClientApi;
+import org.zaproxy.clientapi.core.ClientApiException;
+
+
+/**
+ * This file was automatically generated.
+ */
+public class Selenium {
+
+	private ClientApi api = null;
+
+	public Selenium(ClientApi api) {
+		this.api = api;
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse optionChromeDriverPath() throws ClientApiException {
+		Map<String, String> map = null;
+		return api.callApi("selenium", "view", "optionChromeDriverPath", map);
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse optionIeDriverPath() throws ClientApiException {
+		Map<String, String> map = null;
+		return api.callApi("selenium", "view", "optionIeDriverPath", map);
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse optionPhantomJsBinaryPath() throws ClientApiException {
+		Map<String, String> map = null;
+		return api.callApi("selenium", "view", "optionPhantomJsBinaryPath", map);
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse setOptionChromeDriverPath(String apikey, String string) throws ClientApiException {
+		Map<String, String> map = null;
+		map = new HashMap<String, String>();
+		if (apikey != null) {
+			map.put("apikey", apikey);
+		}
+		map.put("String", string);
+		return api.callApi("selenium", "action", "setOptionChromeDriverPath", map);
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse setOptionIeDriverPath(String apikey, String string) throws ClientApiException {
+		Map<String, String> map = null;
+		map = new HashMap<String, String>();
+		if (apikey != null) {
+			map.put("apikey", apikey);
+		}
+		map.put("String", string);
+		return api.callApi("selenium", "action", "setOptionIeDriverPath", map);
+	}
+
+	/**
+	 * This component is optional and therefore the API will only work if it is installed
+	 */
+	public ApiResponse setOptionPhantomJsBinaryPath(String apikey, String string) throws ClientApiException {
+		Map<String, String> map = null;
+		map = new HashMap<String, String>();
+		if (apikey != null) {
+			map.put("apikey", apikey);
+		}
+		map.put("String", string);
+		return api.callApi("selenium", "action", "setOptionPhantomJsBinaryPath", map);
+	}
+
+}
