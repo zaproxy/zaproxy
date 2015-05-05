8ee9de872bbb6177c4ccfa072c50f733cc1ed494
thc202 <thc202@gmail.com>
2015-05-05 04:05:49 +0000

    Merge revisions 6098 and 6099, both from trunk into branch 2.4
     - HostProcess: Add missing ZAP comment for Issue 1566
     - Add the issues merged to branch 2.4 to 2.4.0.1 release help page

--- src/org/parosproxy/paros/core/scanner/HostProcess.java
+++ src/org/parosproxy/paros/core/scanner/HostProcess.java
@@ -48,6 +48,7 @@
 // ZAP: 2014/11/19 Issue 1412: Manage scan policies
 // ZAP: 2015/02/18 Issue 1062: Tidied up extension hooks
 // ZAP: 2015/04/17 A problem occur when a single node should be scanned because count start from -1
+// ZAP: 2015/05/04 Issue 1566: Improve active scan's reported progress
 
 package org.parosproxy.paros.core.scanner;
 
