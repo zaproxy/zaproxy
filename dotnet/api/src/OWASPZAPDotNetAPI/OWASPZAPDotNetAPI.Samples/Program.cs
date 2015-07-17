b6c5c8e0d1514adb6d6c61f18f6fc709062c3a63
gmaran23 <gmaran23@yahoo.com>
2015-07-17 21:53:20 +0530

    ****OWASP ZAP Dot Net API releated changes*** Set OWASPZAPDotNetAPI.Samples as a start up project Added comments to the start file Program.cs in OWASPZAPDotNetAPI.Samples Added Apache 2.0 license information to all the source files in OWASPZAPDotNetAPI.Samples and OWASPZAPDotNetAPI.Tests

--- dotnet/api/src/OWASPZAPDotNetAPI/OWASPZAPDotNetAPI.Samples/Program.cs
+++ dotnet/api/src/OWASPZAPDotNetAPI/OWASPZAPDotNetAPI.Samples/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/* Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright the ZAP development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +30,15 @@ namespace OWASPZAPDotNetAPI.Samples
     {
         static void Main(string[] args)
         {
-            ZAP.StartZapUI();
+            Console.WriteLine("ZAP.StartZapUI() helps you get started on how to start ZAP UI (that is the ZAP Desktop)" + Environment.NewLine);
+            //ZAP.StartZapUI();
+            Console.WriteLine("ZAP.StartZAPDaemon() helps you get started on how to start ZAP in daemon mode (that is the headless mode - without the UI)" + Environment.NewLine);
             //ZAP.StartZAPDaemon();
+            Console.WriteLine("SimplePointAndClickScan.Go() helps you get started on scanning a website just by providing a URL" + Environment.NewLine);
             //SimplePointAndClickScan.Go();
+            Console.WriteLine("AuthenticatedScanWithFormsAuthentication.Go() helps you get started on scanning a website that has a Login page and uses Forms based authentication" + Environment.NewLine);
             //AuthenticatedScanWithFormsAuthentication.Go();
+            Console.WriteLine("More samples enroute" + Environment.NewLine);
         }
     }
 }
