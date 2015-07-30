74f5e8888bd39fa5774bbc80b82bbc52d8d83099
thc202 <thc202@gmail.com>
2015-07-30 16:34:53 +0100

    Fix i18n of tree root node in active scan's technology panel
    Replace literal string with the i18n'ed text and remove redundant code
    that was creating the tree nodes (the method setTech() is already doing
    that).

--- src/org/zaproxy/zap/extension/ascan/CustomScanDialog.java
+++ src/org/zaproxy/zap/extension/ascan/CustomScanDialog.java
@@ -427,7 +427,7 @@ public class CustomScanDialog extends StandardFieldsDialog {
         TechSet ts = new TechSet(Tech.builtInTech);
         Iterator<Tech> iter = ts.getIncludeTech().iterator();
 
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Technology");
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(Constant.messages.getString("ascan.custom.tab.tech.node"));
         Tech tech;
         DefaultMutableTreeNode parent;
         DefaultMutableTreeNode node;
@@ -670,33 +670,6 @@ public class CustomScanDialog extends StandardFieldsDialog {
                     }
                 }
             };
-            // Initialise the structure based on all the tech we know about
-            TechSet ts = new TechSet(Tech.builtInTech);
-            Iterator<Tech> iter = ts.getIncludeTech().iterator();
-
-            DefaultMutableTreeNode root = new DefaultMutableTreeNode(Constant.messages.getString("ascan.custom.tab.tech.node"));
-            Tech tech;
-            DefaultMutableTreeNode parent;
-            DefaultMutableTreeNode node;
-            while (iter.hasNext()) {
-                tech = iter.next();
-                if (tech.getParent() != null) {
-                    parent = techToNodeMap.get(tech.getParent());
-                } else {
-                    parent = null;
-                }
-                if (parent == null) {
-                    parent = root;
-                }
-                node = new DefaultMutableTreeNode(tech.getUiName());
-                parent.add(node);
-                techToNodeMap.put(tech, node);
-            }
-
-            techModel = new DefaultTreeModel(root);
-            techTree.setModel(techModel);
-            techTree.expandAll();
-            techTree.setCheckBoxEnabled(new TreePath(root), false);
             this.setTech();
 
         }
