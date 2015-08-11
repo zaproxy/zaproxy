0ad70a332cdd6943123a5e4320bd15c902d0e074
thc202 <thc202@gmail.com>
2015-08-11 15:34:33 +0100

    Passive scan AJAX spider requests
    Change PassiveScanThread to include the type of AJAX Spider requests for
    scanning.
    Fix #1778 - Passive scan AJAX spider requests

--- src/org/zaproxy/zap/extension/pscan/PassiveScanThread.java
+++ src/org/zaproxy/zap/extension/pscan/PassiveScanThread.java
@@ -103,7 +103,8 @@ public class PassiveScanThread extends Thread implements ProxyListener, SessionC
 				if (href != null && 
 						(href.getHistoryType() == HistoryReference.TYPE_PROXIED ||
 						href.getHistoryType() == HistoryReference.TYPE_ZAP_USER ||
-						href.getHistoryType() == HistoryReference.TYPE_SPIDER)) {
+						href.getHistoryType() == HistoryReference.TYPE_SPIDER ||
+						href.getHistoryType() == HistoryReference.TYPE_SPIDER_AJAX)) {
 					try {
 						// Note that scanning TYPE_SCANNER records will result in a loop ;)
 						// Parse the record
