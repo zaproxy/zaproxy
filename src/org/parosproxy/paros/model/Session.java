6db4d67990394005eeaf35fed1f825efcdd84672
thc202 <thc202@gmail.com>
2015-08-20 01:00:10 +0100

    Change Session to load Forced Browse/AJAX Spider messages to Sites tree
    Add the Forced Browse/AJAX Spider history reference types to existing
    call that obtains from DB the IDs of the messages that are added to
    Sites tree, when the session is loaded.
    Fix #1789 - Forced Browse/AJAX Spider messages not restored to Sites tab

--- src/org/parosproxy/paros/model/Session.java
+++ src/org/parosproxy/paros/model/Session.java
@@ -51,6 +51,7 @@
 // ZAP: 2015/02/09 Issue 1525: Introduce a database interface layer to allow for alternative implementations
 // ZAP: 2015/04/02 Issue 321: Support multiple databases and Issue 1582: Low memory option
 // ZAP: 2015/08/19 Change to use ZapXmlConfiguration instead of extending FileXML
+// ZAP: 2015/08/19 Issue 1789: Forced Browse/AJAX Spider messages not restored to Sites tab
 
 package org.parosproxy.paros.model;
 
@@ -334,7 +335,8 @@ public class Session {
 		}
 		
 		// update siteTree reference
-		list = model.getDb().getTableHistory().getHistoryIdsOfHistType(getSessionId(), HistoryReference.TYPE_SPIDER);
+		list = model.getDb().getTableHistory().getHistoryIdsOfHistType(getSessionId(), HistoryReference.TYPE_SPIDER,
+				HistoryReference.TYPE_BRUTE_FORCE, HistoryReference.TYPE_SPIDER_AJAX);
 		
 		for (int i=0; i<list.size(); i++) {
 			// ZAP: Removed unnecessary cast.
