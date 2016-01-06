46aa06fff160c3a5fd98222655f90a0913909e5f
thc202 <thc202@gmail.com>
2016-01-06 23:55:11 +0000

    Fix exception and notify changes done to ContextPanelUsersSelectComboBox
    Change class UsersListModel (used by ContextPanelUsersSelectComboBox) to
    fix an exception and do proper notification of changes when setting
    custom users:
    
     - An ArrayIndexOutOfBoundsException is thrown when all users are
     removed from the table model (source of the shown users), which happens
     when changing authentication methods with existing users. The exception
     happened because the UsersTableModel sends a change notification when
     removing all users which the code in UsersListModel did not handled
     correctly (it tried to select a user that didn't exist). Issue
     introduced with changes done in #2143;
    
     - Changes done to custom users are not notified. Change method
     setCustomUsers(User[]) to notify of insertion, modification and removal
     of the custom users.
    
     - Index returned by getIndexOf(Object) for custom users is not correct.
     Change the method to return the index of the custom users taking into
     account the existing table users (which are always before the custom
     users).
    
    Remove redundant initialisation of instance variable in UsersTableModel.
    Extract the static nested class UsersListModel to ease the unit tests.
    Add tests to assert the expected behaviour of UsersTableModel,
    UsersListModel and AbstractMultipleOptionsBaseTableModel.
    Related to #2090 - Context: Rename User, can't select in Forced User
    Panel

---
+++ test/org/zaproxy/zap/view/ListModelTestUtils.java
@@ -0,0 +1,100 @@
+/*
+ * Zed Attack Proxy (ZAP) and its related class files.
+ * 
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ * 
+ * Copyright 2016 The ZAP Development Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zaproxy.zap.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+/**
+ * Class with helper/utility methods to help testing classes involving {@code ListModel} implementations.
+ *
+ * @see javax.swing.ListModel
+ */
+public class ListModelTestUtils {
+
+    public static TestListDataListener createTestListDataListener() {
+        return new TestListDataListener();
+    }
+
+    public static class TestListDataListener implements ListDataListener {
+
+        private final List<ListDataEvent> events;
+
+        private TestListDataListener() {
+            events = new ArrayList<>();
+        }
+
+        @Override
+        public void intervalAdded(ListDataEvent e) {
+            events.add(e);
+        }
+
+        @Override
+        public void intervalRemoved(ListDataEvent e) {
+            events.add(e);
+        }
+
+        @Override
+        public void contentsChanged(ListDataEvent e) {
+            events.add(e);
+        }
+
+        public int getNumberOfEvents() {
+            return events.size();
+        }
+
+        public boolean isListItemAdded(int index) {
+            for (ListDataEvent event : events) {
+                if (ListDataEvent.INTERVAL_ADDED == event.getType()) {
+                    if (index >= event.getIndex0() && index <= event.getIndex1()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean isListItemChanged(int index) {
+            for (ListDataEvent event : events) {
+                if (ListDataEvent.CONTENTS_CHANGED == event.getType()) {
+                    if (index >= event.getIndex0() && index <= event.getIndex1()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean isListItemRemoved(int index) {
+            for (ListDataEvent event : events) {
+                if (ListDataEvent.INTERVAL_REMOVED == event.getType()) {
+                    if (index >= event.getIndex0() && index <= event.getIndex1()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
