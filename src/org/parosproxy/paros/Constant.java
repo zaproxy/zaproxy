2b8649ddc5e214d9b453a3895c9dceda332f57f6
thc202 <thc202@gmail.com>
2016-02-17 21:59:48 +0000

    Convert options to not use extensions' names as XML element names
    Prevent exceptions when the name of the extensions are not valid XML
    element names.
    Add class ExtensionParam that takes care to save/load the configurations
    instead of doing that in the class OptionsExtensionPanel (now using the
    former).
    Change Constant class to convert to the new options when updating from
    version 2.4.3.
    Change class ExtensionExtension to install/register ExtensionParam.
    The configurations now use specific elements, "name" and "enabled", to
    save the name and enabled state of the extension, each one in its own
    "extension" element (which are all under an "extensions" element).

--- src/org/parosproxy/paros/Constant.java
+++ src/org/parosproxy/paros/Constant.java
@@ -60,6 +60,7 @@
 // ZAP: 2015/08/01 Remove code duplication in catch of exceptions, use installation directory in default config file
 // ZAP: 2015/11/11 Issue 2045: Dont copy old configs if -dir option used 
 // ZAP: 2015/11/26 Issue 2084: Warn users if they are probably using out of date versions
+// ZAP: 2016/02/17 Convert extensions' options to not use extensions' names as XML element names
 
 package org.parosproxy.paros;
 
@@ -118,9 +119,10 @@ public final class Constant {
     public static final String ALPHA_VERSION = "alpha";
     public static final String BETA_VERSION = "beta";
     
-    private static final long VERSION_TAG = 2004002;
+    private static final long VERSION_TAG = 2004003;
     
     // Old version numbers - for upgrade
+    private static final long V_2_4_3_TAG = 2004003;
     private static final long V_2_3_1_TAG = 2003001;
     private static final long V_2_2_0_TAG = 2002000;
     private static final long V_2_1_0_TAG = 2001000;
@@ -505,6 +507,9 @@ public final class Constant {
 	            	if (ver <= V_2_3_1_TAG) {
 	            		upgradeFrom2_3_1(config);
 	            	}
+                    if (ver <= V_2_4_3_TAG) {
+                        upgradeFrom2_4_3(config);
+                    }
 	            	log.info("Upgraded from " + ver);
             		
             		// Update the version
@@ -763,6 +768,28 @@ public final class Constant {
         // Remove old authentication options no longer used
         config.clearProperty("connection.confirmRemoveAuth");
         config.clearTree("options.auth");
+    }
+
+    private void upgradeFrom2_4_3(XMLConfiguration config) {
+        List<Object[]> oldData = new ArrayList<>();
+        // Convert extensions' options to not use extensions' names as XML element names
+        for (Iterator<String> it = config.getKeys("ext"); it.hasNext();) {
+            String key = it.next();
+
+            Object[] data = new Object[2];
+            data[0] = key.substring(4);
+            data[1] = config.getBoolean(key);
+            oldData.add(data);
+        }
+        config.clearTree("ext");
+
+        for (int i = 0, size = oldData.size(); i < size; ++i) {
+            String elementBaseKey = "extensions.extension(" + i + ").";
+            Object[] data = oldData.get(i);
+
+            config.setProperty(elementBaseKey + "name", data[0]);
+            config.setProperty(elementBaseKey + "enabled", data[1]);
+        }
     }
 
 	public static void setLocale (String loc) {
