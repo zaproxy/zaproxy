a59348a554592588ecac8d21c81a03ec56498664
Mustafa Abdelhamid <abdelha5@uwm.edu>
2016-04-15 20:59:26 -0500

    Fixes IOException caused by no diskspace This closes #2127 by including the following changes: -Added a method to check for disk space prior to saving file -Added GUI Dialogs to alert user of failed report generation

--- src/org/zaproxy/zap/view/widgets/WritableFileChooser.java
+++ src/org/zaproxy/zap/view/widgets/WritableFileChooser.java
@@ -13,6 +13,8 @@ import org.parosproxy.paros.model.Model;
 public class WritableFileChooser extends JFileChooser {
 
 	private static final long serialVersionUID = -8600149638325315049L;
+	
+	private static final int MINIMUM_SPACE_REQUIREMENT_MB = 5000000;
 
 	public WritableFileChooser() {
 		super();
@@ -26,11 +28,26 @@ public class WritableFileChooser extends JFileChooser {
 	public void approveSelection() {
 		File selectedFile = getSelectedFile();
 
+		File checkFile = selectedFile;
+		boolean fileExists = checkFile.exists();
+		if (!fileExists) {
+			checkFile = checkFile.getParentFile();
+		}
+		if (checkFile.getUsableSpace() < MINIMUM_SPACE_REQUIREMENT_MB) {
+			int result = JOptionPane.showConfirmDialog(this,
+					Constant.messages.getString("report.write.diskspace.warning.dialog.message"),
+					Constant.messages.getString("report.write.diskspace.warning.dialog.title"),
+					JOptionPane.YES_NO_OPTION);
+			if (result != JOptionPane.YES_OPTION) {
+				return;
+			}
+		}
 		if (!Files.isWritable(selectedFile.getParentFile().toPath())) {
 			warnNotWritable("report.write.permission.dir.dialog.message", selectedFile.getParentFile().getAbsolutePath());
+
 			return;
 		}
-		if (selectedFile.exists()) {
+		if (fileExists) {
 			if (!Files.isWritable(selectedFile.toPath())) {
 				warnNotWritable("report.write.permission.file.dialog.message", selectedFile.getAbsolutePath());
 				return;
