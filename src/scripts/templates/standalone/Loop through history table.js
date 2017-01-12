e0f6fb523b96b2b1c89648123754e15879e3f38e
kingthorin <kingthorin@users.noreply.github.com>
2017-01-11 20:20:10 -0500

    Issue 3043 Add helper for printing to JS templates
    Add helper code that overcomes the lack of println when scripts are run
    using Java 8's Nashorn JS engine, by reassigning the function when
    undefined.
    
    Fixes zaproxy/zaproxy#3043

--- src/scripts/templates/standalone/Loop through history table.js
+++ src/scripts/templates/standalone/Loop through history table.js
@@ -3,6 +3,10 @@
 // Standalone scripts have no template.
 // They are only evaluated when you run them. 
 
+// The following handles differences in printing between Java 7's Rhino JS engine
+// and Java 8's Nashorn JS engine
+if (typeof println == 'undefined') this.println = print;
+
 extHist = org.parosproxy.paros.control.Control.getSingleton().
     getExtensionLoader().getExtension(
         org.parosproxy.paros.extension.history.ExtensionHistory.NAME) 
