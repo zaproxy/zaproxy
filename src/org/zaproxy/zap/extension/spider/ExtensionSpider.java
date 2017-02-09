df23c808999e4883825691696e7559faa104288a
kingthorin <kingthorin@users.noreply.github.com>
2017-02-08 21:26:09 -0500

    Fix Adv Spider Options Render
    A minor dimension adjustment to ensure that the Spider Advanced Options
    tab isn't truncated.

--- src/org/zaproxy/zap/extension/spider/ExtensionSpider.java
+++ src/org/zaproxy/zap/extension/spider/ExtensionSpider.java
@@ -801,7 +801,7 @@ public class ExtensionSpider extends ExtensionAdaptor implements SessionChangedL
 
 	public void showSpiderDialog(SiteNode node) {
 		if (spiderDialog == null) {
-			spiderDialog = new SpiderDialog(this, View.getSingleton().getMainFrame(), new Dimension(700, 400));
+			spiderDialog = new SpiderDialog(this, View.getSingleton().getMainFrame(), new Dimension(700, 430));
 		}
 		if (spiderDialog.isVisible()) {
 			// Its behind you! Actually not needed no the window is alwaysOnTop, but keeping in case we change that ;)
