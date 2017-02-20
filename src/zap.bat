5a69cfe1e088c8062c115973a963287a6a27d287
thc202 <thc202@gmail.com>
2017-02-20 08:39:48 +0000

    Use home drive environment variable in zap.bat
    Change zap.bat file to use the environment variable %HOMEDRIVE%, to
    ensure it's used the expected drive/path when checking if the JVM
    properties file exists.
    
    Fix #3211 - Can't find .ZAP_JVM.properties with %HOMEPATH% when using
    zap.bat in windows

--- src/zap.bat
+++ src/zap.bat
@@ -1,5 +1,5 @@
-if exist "%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties" (
-	set /p jvmopts=< "%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties"
+if exist "%HOMEDRIVE%%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties" (
+	set /p jvmopts=< "%HOMEDRIVE%%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties"
 ) else (
 	set jvmopts=-Xmx512m
 )
