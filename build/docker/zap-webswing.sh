10c91e5a7a58104561ba7a47adff0df7736475ee
Jon P <contact@jonathan.pl>
2017-07-20 18:02:23 +0200

    Clear dockerfiles

--- build/docker/zap-webswing.sh
+++ build/docker/zap-webswing.sh
@@ -1,42 +1,41 @@
 #!/bin/sh
 #
 # Startup script for the Webswing
-# 
+#
 # Customised for ZAP running in Docker - just call with no parameters for webswing to start and leave
 # the docker container running
 #
 # Set environment.
 export HOME=/zap/webswing-2.3/
 export OPTS="-h 0.0.0.0 -j $HOME/jetty.properties -u $HOME/user.properties -c $HOME/webswing.config"
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export JAVA_OPTS="-Xmx128M"
 export LOG=$HOME/webswing.out
 export PID_PATH_NAME=$HOME/webswing.pid
 
-if [ -z `command -v $0` ]; then 
+if [ -z `command -v $0` ]; then
     CURRENTDIR=`pwd`
     cd `dirname $0` > /dev/null
     SCRIPTPATH=`pwd`/
     cd $CURRENTDIR
 else
-    SCRIPTPATH="" 
+    SCRIPTPATH=""
 fi
 
 if [ ! -f $HOME/webswing-server.war ]; then
-    echo "Webswing executable not found in $HOME folder" 
+    echo "Webswing executable not found in $HOME folder"
     exit 1
 fi
 
 if [ ! -f $JAVA_HOME/bin/java ]; then
-    echo "Java installation not found in $JAVA_HOME folder" 
+    echo "Java installation not found in $JAVA_HOME folder"
     exit 1
 fi
 if [ -z `command -v xvfb-run` ]; then
-    echo "Unable to locate xvfb-run command. Please install Xvfb before starting Webswing." 
+    echo "Unable to locate xvfb-run command. Please install Xvfb before starting Webswing."
     exit 1
 fi
 if [ ! -z `command -v ldconfig` ]; then
-    if [ `ldconfig -p | grep -i libXext | wc -l` -lt 1 ]; then 
+    if [ `ldconfig -p | grep -i libXext | wc -l` -lt 1 ]; then
         echo "Missing dependent library libXext."
         exit 1
     fi
