ec29b3042402f03374c0caed6c0342219a19eb81
Michael Peters <mipeter3@cisco.com>
2017-10-05 11:00:26 -0400

    Remove info log and just use a comment

--- build/docker/zap_common.py
+++ build/docker/zap_common.py
@@ -374,8 +374,8 @@ def get_latest_zap_client_version():
 
 
 def check_zap_client_version():
+    # No need to check ZAP Python API client while running in Docker
     if running_in_docker():
-        logging.info('No need to check ZAP Python API client while running in Docker')
         return
 
     if 'pkg_resources' not in globals():  # import failed
