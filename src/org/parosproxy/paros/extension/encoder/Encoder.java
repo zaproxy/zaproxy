685dabda393df02099c3cde4203cb427fed7286e
usualwyy <usualwyy@163.com>
2017-12-06 12:32:36 +0800

    Add unicode functions.

--- src/org/parosproxy/paros/extension/encoder/Encoder.java
+++ src/org/parosproxy/paros/extension/encoder/Encoder.java
@@ -38,7 +38,6 @@ import java.security.NoSuchAlgorithmException;
 
 import org.apache.log4j.Logger;
 
-import java.lang.*;
 
 public class Encoder {
     
@@ -155,82 +154,6 @@ public class Encoder {
 		
 		return sbResult.toString();
 	}
-
-
-	public String getUnicodeEncode(String str) {
-        str = (str == null ? "" : str);
-        String tmp;
-        StringBuffer sb = new StringBuffer(1000);
-        char c;
-        int i, j;
-        sb.setLength(0);
-        for (i = 0; i < str.length(); i++)
-        {
-            c = str.charAt(i);
-            sb.append("%u");
-            j = (c >>>8); //取出高8位
-            tmp = Integer.toHexString(j);
-            if (tmp.length() == 1)
-                sb.append("0");
-            sb.append(tmp);
-            j = (c & 0xFF); //取出低8位
-            tmp = Integer.toHexString(j);
-            if (tmp.length() == 1)
-                sb.append("0");
-            sb.append(tmp);
-
-        }
-        return (new String(sb));
-    }
-
-    public String getUnicodeDecode(String str) {
-        str = (str == null ? "" : str);
-        if (str.indexOf("%u") == -1)//如果不是unicode码则原样返回
-            return str;
-
-        StringBuffer sb = new StringBuffer(1000);
-
-        for (int i = 0; i <= str.length() - 6;)
-        {
-            String strTemp = str.substring(i, i + 6);
-            String value = strTemp.substring(2);
-            int c = 0;
-            for (int j = 0; j < value.length(); j++)
-            {
-                char tempChar = value.charAt(j);
-                int t = 0;
-                switch (tempChar)
-                {
-                    case 'a':
-                        t = 10;
-                        break;
-                    case 'b':
-                        t = 11;
-                        break;
-                    case 'c':
-                        t = 12;
-                        break;
-                    case 'd':
-                        t = 13;
-                        break;
-                    case 'e':
-                        t = 14;
-                        break;
-                    case 'f':
-                        t = 15;
-                        break;
-                    default:
-                        t = tempChar - 48;
-                        break;
-                }
-
-                c += t * ((int) Math.pow(16, (value.length() - j - 1)));
-            }
-            sb.append((char) c);
-            i = i + 6;
-        }
-        return sb.toString();
-    }
 	
 	public void setBase64DoBreakLines(boolean doBreakLines) {
 		if (doBreakLines) {
