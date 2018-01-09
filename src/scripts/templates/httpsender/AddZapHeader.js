ebf4cd09e8670c480cdf5c99835d21892d28642a
kingthorin <kingthorin@users.noreply.github.com>
2018-01-08 19:01:49 -0500

    Issue 1429 - Add script template for ZAP Header
    AddZapHeader.js > New template which adds a ZAP header to all requests.
    eg: `X-ZAP-Initiator: 3`
    
    Fixes zaproxy/zaproxy#1429

---
+++ src/scripts/templates/httpsender/AddZapHeader.js
@@ -0,0 +1,23 @@
+// The sendingRequest and responseReceived functions will be called for all requests/responses sent/received by ZAP, 
+// including automated tools (e.g. active scanner, fuzzer, ...)
+
+// Note that new HttpSender scripts will initially be disabled
+// Right click the script in the Scripts tree and select "enable"  
+
+// For the latest list of 'initiator' values see the HttpSender class:
+// https://github.com/zaproxy/zaproxy/blob/master/src/org/parosproxy/paros/network/HttpSender.java
+// 'helper' just has one method at the moment: helper.getHttpSender() which returns the HttpSender 
+// instance used to send the request.
+
+// In order to facilitate identifying ZAP traffic and Web Application Firewall exceptions, ZAP is accompanied 
+// by this script which can be used to add a specific header to all traffic that passes through 
+// or originates from ZAP. eg: X-ZAP-Initiator: 3
+
+function sendingRequest(msg, initiator, helper) {
+	// Add a ZAP identifier header to all traffic that originates with or passes through ZAP
+	msg.getRequestHeader().setHeader("X-ZAP-Initiator", initiator);
+}
+
+function responseReceived(msg, initiator, helper) {
+	// Nothing to do here
+}
