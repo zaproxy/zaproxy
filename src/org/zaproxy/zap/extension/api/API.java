3dddc749f98576dcc372d4b149985769356682d5
thc202 <thc202@gmail.com>
2018-03-26 23:42:34 +0100

    Include cause of failed API format/type conversion
    Change API to include the exception/cause when throwing the ApiException
    when failed to convert the format/type.

--- src/org/zaproxy/zap/extension/api/API.java
+++ src/org/zaproxy/zap/extension/api/API.java
@@ -365,7 +365,7 @@ public class API {
 						}
 					} catch (IllegalArgumentException e) {
 						format = Format.HTML;
-						throw new ApiException(ApiException.Type.BAD_FORMAT);
+						throw new ApiException(ApiException.Type.BAD_FORMAT, e);
 					}
 				}
 				if (elements.length > 4) {
@@ -379,7 +379,7 @@ public class API {
 					try {
 						reqType = RequestType.valueOf(elements[5]);
 					} catch (IllegalArgumentException e) {
-						throw new ApiException(ApiException.Type.BAD_TYPE);
+						throw new ApiException(ApiException.Type.BAD_TYPE, e);
 					}
 				}
 				if (elements.length > 6) {
