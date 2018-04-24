181384e322c008aa49c003233c31909c39b1f4a8
thc202 <thc202@gmail.com>
2018-04-24 19:33:58 +0100

    Add JSON-based authentication method
    Add an authentication method, JsonBasedAuthenticationMethodType, that
    allows to send a JSON object in the request body. It can be configured
    through the API with the name "jsonBasedAuthentication".
    Extract a class, PostBasedAuthenticationMethodType, to be used by
    FormBasedAuthenticationMethodType and the new auth method, the behaviour
    is exactly the same except for the data they handle (application/json or
    application/x-www-form-urlencoded) and the POST data required.
    Add libraries used to quote the authentication credentials when injected
    into the request body.
    Add JSON Content-Type to HttpHeader.
    
    Fix #2439 - Configure Authentication with Json object

--- src/org/zaproxy/zap/extension/authentication/ExtensionAuthentication.java
+++ src/org/zaproxy/zap/extension/authentication/ExtensionAuthentication.java
@@ -44,6 +44,7 @@ import org.zaproxy.zap.authentication.AuthenticationMethodType;
 import org.zaproxy.zap.authentication.FormBasedAuthenticationMethodType;
 import org.zaproxy.zap.authentication.FormBasedAuthenticationMethodType.FormBasedAuthenticationMethod;
 import org.zaproxy.zap.authentication.HttpAuthenticationMethodType;
+import org.zaproxy.zap.authentication.JsonBasedAuthenticationMethodType;
 import org.zaproxy.zap.authentication.ManualAuthenticationMethodType;
 import org.zaproxy.zap.authentication.ScriptBasedAuthenticationMethodType;
 import org.zaproxy.zap.extension.stdmenus.PopupContextMenuItemFactory;
@@ -202,6 +203,7 @@ public class ExtensionAuthentication extends ExtensionAdaptor implements Context
 		this.authenticationMethodTypes.add(new HttpAuthenticationMethodType());
 		this.authenticationMethodTypes.add(new ManualAuthenticationMethodType());
 		this.authenticationMethodTypes.add(new ScriptBasedAuthenticationMethodType());
+		this.authenticationMethodTypes.add(new JsonBasedAuthenticationMethodType());
 
 		for (AuthenticationMethodType a : authenticationMethodTypes) {
 			a.hook(hook);
