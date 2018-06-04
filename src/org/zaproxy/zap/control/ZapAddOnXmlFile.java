ee9d3ccc0f6eca232ff2275172b1f9679c647a0c
thc202 <thc202@gmail.com>
2018-06-04 23:39:30 +0100

    Allow add-ons to declare a resource bundle
    Add-ons can now declare a resource bundle through the manifest file:
    ```XML
    <bundle prefix="example">com.example.Messages</bundle>
    ```
    the prefix is used to add the ResourceBundle into the I18N instance in
    the Constant class. The bundle is also accessible through the add-on
    itself.
    
    The extensions of the add-on will use the declared bundle if none is
    found in the default location.
    
    Fix #3841 - Allow add-ons without extensions to have resource messages

--- src/org/zaproxy/zap/control/ZapAddOnXmlFile.java
+++ src/org/zaproxy/zap/control/ZapAddOnXmlFile.java
@@ -38,10 +38,15 @@ public class ZapAddOnXmlFile extends BaseZapAddOnXmlData {
     private static final String PSCANRULES_ALL_ELEMENTS = "pscanrules/" + PSCANRULE_ELEMENT;
     private static final String FILE_ELEMENT = "file";
     private static final String FILES_ALL_ELEMENTS = "files/" + FILE_ELEMENT;
+    private static final String BUNDLE_ELEMENT = "bundle";
+    private static final String BUNDLE_PREFIX_ATT = "bundle/@prefix";
 
     private List<String> ascanrules;
     private List<String> pscanrules;
     private List<String> files;
+
+    private String bundleBaseName;
+    private String bundlePrefix;
 
     public ZapAddOnXmlFile(InputStream is) throws IOException {
         super(is);
@@ -52,6 +57,9 @@ public class ZapAddOnXmlFile extends BaseZapAddOnXmlData {
         ascanrules = getStrings(zapAddOnXml, ASCANRULES_ALL_ELEMENTS, ASCANRULE_ELEMENT);
         pscanrules = getStrings(zapAddOnXml, PSCANRULES_ALL_ELEMENTS, PSCANRULE_ELEMENT);
         files = getStrings(zapAddOnXml, FILES_ALL_ELEMENTS, FILE_ELEMENT);
+
+        bundleBaseName = zapAddOnXml.getString(BUNDLE_ELEMENT, "");
+        bundlePrefix = zapAddOnXml.getString(BUNDLE_PREFIX_ATT, "");
     }
 
     public List<String> getAscanrules() {
@@ -64,5 +72,25 @@ public class ZapAddOnXmlFile extends BaseZapAddOnXmlData {
 
     public List<String> getFiles() {
         return files;
+    }
+
+    /**
+     * Gets the base name of the bundle.
+     *
+     * @return the base name of the bundle, never {@code null}.
+     * @since TODO add version
+     */
+    public String getBundleBaseName() {
+        return bundleBaseName;
+    }
+
+    /**
+     * Gets the prefix of the bundle.
+     *
+     * @return the prefix of the bundle, never {@code null}.
+     * @since TODO add version
+     */
+    public String getBundlePrefix() {
+        return bundlePrefix;
     }
 }
