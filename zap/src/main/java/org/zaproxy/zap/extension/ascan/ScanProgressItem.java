37d62bf89bd8f0de0e116866e7e0d3b35eec03c9
Josh Soref <jsoref@users.noreply.github.com>
2019-11-26 18:08:06 -0500

    spelling: [API] strength

--- zap/src/main/java/org/zaproxy/zap/extension/ascan/ScanProgressItem.java
+++ zap/src/main/java/org/zaproxy/zap/extension/ascan/ScanProgressItem.java
@@ -57,8 +57,14 @@ public class ScanProgressItem {
         return pluginStats.getPluginName();
     }
 
-    /** @return */
+    /** @deprecated use {link #getAttackStrengthLabel} */
+    @Deprecated
     public String getAttackStrenghtLabel() {
+        return getAttackStrengthLabel();
+    }
+
+    /** @return */
+    public String getAttackStrengthLabel() {
         return Constant.messages.getString(
                 "ascan.policy.level." + plugin.getAttackStrength().name().toLowerCase(Locale.ROOT));
     }
