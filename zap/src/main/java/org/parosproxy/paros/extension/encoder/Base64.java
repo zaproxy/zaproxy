6b6e88a06d036aac2fe5af7387e8da76e6749705
Josh Soref <jsoref@users.noreply.github.com>
2019-11-26 18:09:32 -0500

    spelling: suspended
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- zap/src/main/java/org/parosproxy/paros/extension/encoder/Base64.java
+++ zap/src/main/java/org/parosproxy/paros/extension/encoder/Base64.java
@@ -1937,7 +1937,7 @@ public class Base64
             if( suspendEncoding ) {
                 this.out.write( theByte );
                 return;
-            }   // end if: supsended
+            }   // end if: suspended
             
             // Encode?
             if( encode ) {
@@ -1992,7 +1992,7 @@ public class Base64
             if( suspendEncoding ) {
                 this.out.write( theBytes, off, len );
                 return;
-            }   // end if: supsended
+            }   // end if: suspended
             
             for( int i = 0; i < len; i++ ) {
                 write( theBytes[ off + i ] );
