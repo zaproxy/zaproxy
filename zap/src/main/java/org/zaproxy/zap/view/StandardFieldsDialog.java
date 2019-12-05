53c38b81d503179d96004b9cf4263f7a1d1f3564
Josh Soref <jsoref@users.noreply.github.com>
2019-12-04 23:20:52 -0500

    Switch cancel button to end
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- zap/src/main/java/org/zaproxy/zap/view/StandardFieldsDialog.java
+++ zap/src/main/java/org/zaproxy/zap/view/StandardFieldsDialog.java
@@ -332,10 +332,10 @@ public abstract class StandardFieldsDialog extends AbstractDialog {
                 contentPanel.add(getHelpButton(helpIndex), LayoutHelper.getGBC(0, 1, 1, 0.0D));
             }
             contentPanel.add(new JLabel(), LayoutHelper.getGBC(1, 1, 1, 1.0D)); // spacer
+            contentPanel.add(getSaveButton(), LayoutHelper.getGBC(2, 1, 1, 0.0D));
             if (hasCancelSaveButtons()) {
-                contentPanel.add(getCancelButton(), LayoutHelper.getGBC(2, 1, 1, 0.0D));
+                contentPanel.add(getCancelButton(), LayoutHelper.getGBC(3, 1, 1, 0.0D));
             }
-            contentPanel.add(getSaveButton(), LayoutHelper.getGBC(3, 1, 1, 0.0D));
         } else {
             contentPanel.add(
                     component, LayoutHelper.getGBC(0, 0, 4 + extraButtons.length, 1.0D, 1.0D));
@@ -343,15 +343,15 @@ public abstract class StandardFieldsDialog extends AbstractDialog {
                 contentPanel.add(getHelpButton(helpIndex), LayoutHelper.getGBC(0, 1, 1, 0.0D));
             }
             contentPanel.add(new JLabel(), LayoutHelper.getGBC(1, 1, 1, 1.0D)); // spacer
-            if (hasCancelSaveButtons()) {
-                contentPanel.add(getCancelButton(), LayoutHelper.getGBC(2, 1, 1, 0.0D));
-            }
+            contentPanel.add(getSaveButton(), LayoutHelper.getGBC(2, 1, 1, 0.0D));
             int x = 3;
             for (JButton button : extraButtons) {
                 contentPanel.add(button, LayoutHelper.getGBC(x, 1, 1, 0.0D));
                 x++;
             }
-            contentPanel.add(getSaveButton(), LayoutHelper.getGBC(x, 1, 1, 0.0D));
+            if (hasCancelSaveButtons()) {
+                contentPanel.add(getCancelButton(), LayoutHelper.getGBC(x, 1, 1, 0.0D));
+            }
         }
     }
 
