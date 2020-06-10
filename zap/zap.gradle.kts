b6be264d8d885d955649a9c3eaf2e1db087e90ad
thc202 <thc202@gmail.com>
2020-06-10 08:22:37 +0100

    Update FlatLaf
    Update to latest version, 0.36, to pick the latest features and fixes.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- zap/zap.gradle.kts
+++ zap/zap.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
         setTransitive(false)
     }
     implementation("org.javadelight:delight-nashorn-sandbox:0.1.26")
-    implementation("com.formdev:flatlaf:0.30")
+    implementation("com.formdev:flatlaf:0.36")
 
     runtimeOnly("commons-jxpath:commons-jxpath:1.3")
     runtimeOnly("commons-logging:commons-logging:1.2")
