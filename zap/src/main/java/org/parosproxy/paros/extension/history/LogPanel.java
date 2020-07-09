846a4d0ef21c1fe97cc230bb491af5e3191c534c
kingthorin <kingthorin@users.noreply.github.com>
2020-07-09 06:49:44 -0400

    LogPanel - Tweaks
    Removed unused variable (and related import) and remove boilerplate
    javadoc for getScrollLog().
    
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- zap/src/main/java/org/parosproxy/paros/extension/history/LogPanel.java
+++ zap/src/main/java/org/parosproxy/paros/extension/history/LogPanel.java
@@ -50,6 +50,8 @@
 // ZAP: 2019/06/01 Normalise line endings.
 // ZAP: 2019/06/05 Normalise format/style.
 // ZAP: 2020/01/02 Allow to set if messages are displayed.
+// ZAP: 2020/07/09 Removed unused variable (and related import) and remove boilerplate javadoc for
+// getScrollLog()
 package org.parosproxy.paros.extension.history;
 
 import java.awt.BorderLayout;
@@ -67,7 +69,6 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreePath;
-import org.apache.log4j.Logger;
 import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.extension.AbstractPanel;
 import org.parosproxy.paros.extension.ViewDelegate;
@@ -86,8 +87,6 @@ import org.zaproxy.zap.view.table.HistoryReferencesTableModel;
 
 public class LogPanel extends AbstractPanel {
     private static final long serialVersionUID = 1L;
-    // ZAP: Added logger.
-    private static final Logger logger = Logger.getLogger(LogPanel.class);
     private javax.swing.JScrollPane scrollLog = null;
     private HistoryTable historyReferencesTable = null;
     // ZAP: Added history (filter) toolbar
@@ -147,11 +146,6 @@ public class LogPanel extends AbstractPanel {
         this.extension = extension;
     }
 
-    /**
-     * This method initializes scrollLog
-     *
-     * @return javax.swing.JScrollPane
-     */
     private javax.swing.JScrollPane getScrollLog() {
         if (scrollLog == null) {
             scrollLog = new javax.swing.JScrollPane();
