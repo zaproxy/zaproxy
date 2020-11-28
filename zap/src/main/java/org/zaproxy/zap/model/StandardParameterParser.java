ea7b88d894f45b5420732492cbc7ba21a1517f48
Jman420 <jgiannone@mdsol.com>
2020-11-27 21:39:32 -0500

    First draft of New DDN Param Parser logic
    Signed-off-by: Jman420 <jgiannone@mdsol.com>
    (cherry picked from commit b1c323e7d1ca6eef50df762374573ef8c3e04256)

--- zap/src/main/java/org/zaproxy/zap/model/StandardParameterParser.java
+++ zap/src/main/java/org/zaproxy/zap/model/StandardParameterParser.java
@@ -330,37 +330,32 @@ public class StandardParameterParser implements ParameterParser {
             if (context != null) {
                 String uriStr = uri.toString();
                 boolean changed = false;
-                for (StructuralNodeModifier ddn : context.getDataDrivenNodes()) {
-                    Matcher m = ddn.getPattern().matcher(uriStr);
-                    if (m.find()) {
-                        if (m.groupCount() == 3) {
-                            path =
-                                    m.group(1)
-                                            + SessionStructure.DATA_DRIVEN_NODE_PREFIX
-                                            + ddn.getName()
-                                            + SessionStructure.DATA_DRIVEN_NODE_POSTFIX
-                                            + m.group(3);
-                            if (!path.startsWith("/")) {
-                                // Should always start with a slash;)
-                                path = "/" + path;
-                            }
-                            changed = true;
-                        } else if (m.groupCount() == 2) {
-                            path =
-                                    m.group(1)
-                                            + SessionStructure.DATA_DRIVEN_NODE_PREFIX
-                                            + ddn.getName()
-                                            + SessionStructure.DATA_DRIVEN_NODE_POSTFIX;
-                            if (!path.startsWith("/")) {
-                                // Should always start with a slash;)
-                                path = "/" + path;
-                            }
-                            changed = true;
-                        }
-                    }
+                
+                List<DataDrivenNode> dataDrivenNodes = context.getDataDrivenNodes_New();
+                List<DataDrivenNode> foundChildNodes = null;
+                while (dataDrivenNodes != null && dataDrivenNodes.size() > 0) {
+                	foundChildNodes = null;
+                	for (DataDrivenNode dataDrivenNode : dataDrivenNodes) {
+                		Pattern ddnPattern = Pattern.compile(dataDrivenNode.getPattern());
+                		Matcher ddnMatcher = ddnPattern.matcher(uriStr);
+                		
+                		if (ddnMatcher.matches()) {
+                			foundChildNodes = dataDrivenNode.getChildNodes();
+                			
+                			if (!dataDrivenNode.getDataNodePattern().isBlank()) {
+                				ddnMatcher.replaceAll(dataDrivenNode.getReplacementPattern());
+                				changed = true;
+                			}
+                		}
+                	}
+                	
+                	dataDrivenNodes = foundChildNodes;
                 }
+                
                 if (changed) {
-                    log.debug("Changed path from " + uri.getPath() + " to " + path);
+                	log.debug("Changed URI from " + uri.toString() + " to " + uriStr);
+                	URI newUri = new URI(uriStr, true);
+                	path = newUri.getPath();
                 }
             }
 
