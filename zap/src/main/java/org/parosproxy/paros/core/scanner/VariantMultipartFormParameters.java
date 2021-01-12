d58aa45a34c9beead9c7a22be2472e26c65aaa87
karan preet singh sasan <karanpreet_singh@intuit.com>
2021-01-12 15:41:43 +0530

    Small Changes
    Signed-off-by: karan preet singh sasan <karanpreet_singh@intuit.com>
    Signed-off-by: Karan Preet Singh Sasan <preetkaran20@gmail.com>

--- zap/src/main/java/org/parosproxy/paros/core/scanner/VariantMultipartFormParameters.java
+++ zap/src/main/java/org/parosproxy/paros/core/scanner/VariantMultipartFormParameters.java
@@ -55,10 +55,11 @@ public class VariantMultipartFormParameters implements Variant {
     // insensitive & DOTALL, and hit "test")
 
     private List<NameValuePair> params = Collections.emptyList();
-    private List<MultipartFormParameter> multiPartParams = new ArrayList<MultipartFormParameter>();
+    private List<MultipartFormParameter> multiPartParams;
 
     @Override
     public void setMessage(HttpMessage msg) {
+        multiPartParams = new ArrayList<MultipartFormParameter>();
         if (msg == null) {
             throw new IllegalArgumentException("Parameter message must not be null.");
         }
