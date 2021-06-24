a4f71797f4e7af89409044e215fe8bc4ca4510c9
thc202 <thc202@gmail.com>
2021-06-24 11:12:18 +0100

    Add Error Prone
    Add Error Prone Gradle plugin.
    Enable the checks `MissingOverride` and `WildcardImport`.
    Remove wildcard import.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
     id("com.diffplug.spotless")
     id("org.sonarqube") version "3.1.1"
     id("com.github.ben-manes.versions") version "0.38.0"
+    id("net.ltgt.errorprone") version "2.0.1"
 }
 
 apply(from = "$rootDir/gradle/ci.gradle.kts")
 
 allprojects {
     apply(plugin = "com.diffplug.spotless")
+    apply(plugin = "net.ltgt.errorprone")
 
     repositories {
         mavenCentral()
@@ -26,9 +30,25 @@ allprojects {
         }
     }
 
+    project.plugins.withType(JavaPlugin::class) {
+        dependencies {
+            "errorprone"("com.google.errorprone:error_prone_core:2.7.1")
+            if (JavaVersion.current() == JavaVersion.VERSION_1_8) {
+                "errorproneJavac"("com.google.errorprone:javac:9+181-r4173-1")
+            }
+        }
+    }
+
     tasks.withType<JavaCompile>().configureEach {
         options.encoding = "utf-8"
         options.compilerArgs = listOf("-Xlint:all", "-Xlint:-options", "-Werror", "-parameters")
+        options.errorprone {
+            disableAllChecks.set(true)
+            error(
+                "MissingOverride",
+                "WildcardImport"
+            )
+        }
     }
 }
 
