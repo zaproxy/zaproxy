4d781198f1b5a551ae4b02e51c37e18089965dc8
Sravan <Sravan-Apps@users.noreply.github.com>
2021-07-16 12:04:34 +0530

    Avoid setting Request Header Content-length:0 in GET requests
    Per RFC we should not add Content-Length header for GET,HEAD,TRACE methods. 
    https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2
    
    Content-Length is valid only  for POST,PUT, PATCH, DELETE methods.
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Method
    
    Signed-off-by: Sravan-Apps <g.sravan4u@gmail.com>

--- zap/src/main/java/org/parosproxy/paros/network/HttpRequestHeader.java
+++ zap/src/main/java/org/parosproxy/paros/network/HttpRequestHeader.java
@@ -60,6 +60,7 @@
 // isSpecificType(Pattern).
 // ZAP: 2020/11/26 Use Log4j 2 classes for logging.
 // ZAP: 2021/05/10 Use authority for CONNECT requests.
+// ZAP: 2021/07/16 Issue #6691: Prevent Spider and other addons setting Request Header Content-length:0 in GET requests
 package org.parosproxy.paros.network;
 
 import java.io.UnsupportedEncodingException;
@@ -243,8 +244,12 @@ public class HttpRequestHeader extends HttpHeader {
 
         setHeader(ACCEPT_ENCODING, null);
 
-        // ZAP: changed from method to version
-        if (version.equalsIgnoreCase(HTTP11)) {
+    // Content-Length is valid only  for POST,PUT, PATCH, DELETE methods
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+    // Per RFC we should not add this header for other methods
+    // https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2
+    if (method.equalsIgnoreCase(POST) || method.equalsIgnoreCase(PUT) || 
+        method.equalsIgnoreCase(PATCH) || method.equalsIgnoreCase(DELETE)) {
             setContentLength(0);
         }
     }
