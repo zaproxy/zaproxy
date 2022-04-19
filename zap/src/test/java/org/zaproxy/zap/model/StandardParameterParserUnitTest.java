adbc02c875ec75de1417df8446dba997012af26b
kiy0taka <kiy0taka333@gmail.com>
2022-04-19 20:26:26 +0900

    fix a format violation
    Signed-off-by: kiy0taka <kiy0taka333@gmail.com>

--- zap/src/test/java/org/zaproxy/zap/model/StandardParameterParserUnitTest.java
+++ zap/src/test/java/org/zaproxy/zap/model/StandardParameterParserUnitTest.java
@@ -19,6 +19,7 @@
  */
 package org.zaproxy.zap.model;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,7 +27,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.List;
