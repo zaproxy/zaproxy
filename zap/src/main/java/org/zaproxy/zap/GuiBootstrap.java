d3c269f6508e6fc56ebdd4be5bf98861d1a98471
thc202 <thc202@gmail.com>
2022-04-26 15:43:20 +0100

    Remove outgoing proxy data prompt at startup
    The network add-on will take care to prompt for the data.
    
    Signed-off-by: thc202 <thc202@gmail.com>
    Signed-off-by: iMinusMinus <mean.leung@outlook.com>

--- zap/src/main/java/org/zaproxy/zap/GuiBootstrap.java
+++ zap/src/main/java/org/zaproxy/zap/GuiBootstrap.java
@@ -54,7 +54,6 @@ import org.zaproxy.zap.model.SessionUtils;
 import org.zaproxy.zap.utils.FontUtils;
 import org.zaproxy.zap.utils.LocaleUtils;
 import org.zaproxy.zap.view.LocaleDialog;
-import org.zaproxy.zap.view.ProxyDialog;
 
 /**
  * The bootstrap process for GUI mode.
@@ -162,8 +161,6 @@ public class GuiBootstrap extends ZapBootstrap {
         }
 
         View.getSingleton().showSplashScreen();
-
-        promptForProxyDetailsIfNeeded(options);
 
         Thread bootstrap =
                 new Thread(
@@ -504,14 +501,6 @@ public class GuiBootstrap extends ZapBootstrap {
         }
 
         return localeBuilder.build();
-    }
-
-    private static void promptForProxyDetailsIfNeeded(OptionsParam options) {
-        if (options.getConnectionParam().isProxyChainPrompt()) {
-            final ProxyDialog dialog = new ProxyDialog(null, true);
-            dialog.init(options);
-            dialog.setVisible(true);
-        }
     }
 
     /**
