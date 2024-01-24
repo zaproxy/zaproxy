90d5c71b66fe43adaca007f81b92b1eca6e3c78a
amitpanwar789 <amitpanwar02705@gmail.com>
2024-01-25 03:11:40 +0530

    fixed commment in extensionHistory file
    Signed-off-by: amitpanwar789 <amitpanwar02705@gmail.com>

--- zap/src/main/java/org/parosproxy/paros/extension/history/ExtensionHistory.java
+++ zap/src/main/java/org/parosproxy/paros/extension/history/ExtensionHistory.java
@@ -105,7 +105,7 @@
 // ZAP: 2023/01/11 Prevent NPE in "showInHistory" when tab doesn't have focus.
 // ZAP: 2023/01/22 Add utility getHistoryIds() method.
 // ZAP: 2023/02/22 Correct delete consistency fix.
-// ZAP: 2024/01/25 Added SiteNotesAddDialog and its utility methods
+// ZAP: 2024/01/25 Added SiteNotesAddDialog and its utility methods (Issue 8240).
 package org.parosproxy.paros.extension.history;
 
 import java.awt.EventQueue;
