21da655426ee1f7464e7d241abb50c90ba160966
kingthorin <kingthorin@users.noreply.github.com>
2024-03-21 13:36:00 -0400

    regex tag scanner: Add initial test for tagging of json responses
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- zap/src/test/java/org/zaproxy/zap/extension/pscan/scanner/RegexAutoTagScannerUnitTest.java
+++ zap/src/test/java/org/zaproxy/zap/extension/pscan/scanner/RegexAutoTagScannerUnitTest.java
@@ -19,23 +19,36 @@
  */
 package org.zaproxy.zap.extension.pscan.scanner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
+import java.io.IOException;
+import java.io.InputStream;
 import net.htmlparser.jericho.Source;
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.parosproxy.paros.db.DatabaseException;
 import org.parosproxy.paros.model.HistoryReference;
+import org.parosproxy.paros.network.HttpHeader;
 import org.parosproxy.paros.network.HttpMalformedHeaderException;
 import org.parosproxy.paros.network.HttpMessage;
+import org.zaproxy.zap.extension.pscan.PassiveScanParam;
 import org.zaproxy.zap.utils.Stats;
 import org.zaproxy.zap.utils.StatsListener;
+import org.zaproxy.zap.utils.ZapXmlConfiguration;
 
 class RegexAutoTagScannerUnitTest {
 
@@ -44,8 +57,20 @@ class RegexAutoTagScannerUnitTest {
     private static final String TEST_PATTERN = ".*foo\\sbar";
     private static final String TEST_CONFIG = "Test";
 
+    private static PassiveScanParam passiveScanParam;
+
     private RegexAutoTagScanner rule;
     private StatsListener listener;
+
+    @BeforeAll
+    static void beforeAll() throws ConfigurationException, IOException {
+        passiveScanParam = new PassiveScanParam();
+        try (InputStream is =
+                RegexAutoTagScannerUnitTest.class.getResourceAsStream(
+                        "/org/zaproxy/zap/resources/config.xml")) {
+            passiveScanParam.load(new ZapXmlConfiguration(is));
+        }
+    }
 
     @BeforeEach
     void setUp() {
@@ -109,5 +134,44 @@ class RegexAutoTagScannerUnitTest {
         rule.scanHttpResponseReceive(msg, -1, new Source(msg.getResponseBody().toString()));
         // Then
         Mockito.verifyNoInteractions(listener);
+    }
+
+    private RegexAutoTagScanner getRegexRuleByName(String taggerName) {
+        for (RegexAutoTagScanner tagRule : passiveScanParam.getAutoTagScanners()) {
+            if (tagRule.getName().equals(taggerName)) {
+                return tagRule;
+            }
+        }
+        return null;
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(
+            strings = {
+                "application/JSON",
+                "application/jSon",
+                "application/json",
+                "aPPlication/json",
+                "application/json; charset=utf-8"
+            })
+    void shouldCountWhenHeaderContainsJsonMatch(String contentType)
+            throws URIException, HttpMalformedHeaderException, DatabaseException {
+        // Given
+        RegexAutoTagScanner rule = getRegexRuleByName("response_json");
+        HttpMessage msg = new HttpMessage(new URI("http://example.com/", true));
+        msg.setHistoryRef(mock(HistoryReference.class));
+        msg.getResponseHeader().setHeader(HttpHeader.CONTENT_TYPE, contentType);
+        // When
+        rule.scanHttpResponseReceive(msg, -1, new Source(msg.getResponseBody().toString()));
+        // Then
+        ArgumentCaptor<String> siteCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> keyCaptor = ArgumentCaptor.forClass(String.class);
+
+        Mockito.verify(listener).counterInc(siteCaptor.capture(), keyCaptor.capture());
+        assertThat(siteCaptor.getValue(), is(equalTo("http://example.com")));
+        assertThat(rule.getConf(), is(equalTo("JSON")));
+        assertThat(
+                keyCaptor.getValue(),
+                is(equalTo(RegexAutoTagScanner.TAG_STATS_PREFIX + rule.getConf())));
     }
 }
