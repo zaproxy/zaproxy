e29399231c9e9dee7c6536f4a6f6593d62a95b14
Simon Bennetts <psiinon@gmail.com>
2024-12-09 09:44:08 +0000

    2.16 Update version
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- zap/src/main/java/org/parosproxy/paros/Constant.java
+++ zap/src/main/java/org/parosproxy/paros/Constant.java
@@ -209,7 +209,7 @@ public final class Constant {
     private static final String VERSION_ELEMENT = "version";
 
     // Accessible for tests
-    static final long VERSION_TAG = 20015000;
+    static final long VERSION_TAG = 20016000;
 
     // Old version numbers - for upgrade
     private static final long V_2_14_0_TAG = 20014000;
