4e8a7cef127fa1fce3753eacccb83bbbaed43879
Simon Bennetts <psiinon@gmail.com>
2025-01-24 09:55:59 +0000

    Itests - use zaproxy instead of example
    Hopefully this will be more stable??
    It appears to be when run locally, but that may change in CI/CD of
    course..
    
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- docker/integration_tests/baseline_tests.sh
+++ docker/integration_tests/baseline_tests.sh
@@ -54,28 +54,28 @@ check_results() {
 
 mkdir -p /zap/wrk/output
 
-echo "TEST: Baseline test 1 (vs example.com)"
-/zap/zap-baseline.py -s -t https://www.example.com/ > /zap/wrk/output/baseline1.out
+echo "TEST: Baseline test 1 (vs zaproxy.com)"
+/zap/zap-baseline.py -s -t https://www.zaproxy.com/ > /zap/wrk/output/baseline1.out
 check_results "baseline1" $? 2
 
 echo
-echo "TEST: Baseline test 2 (vs example.com with INFO/WARN/FAIL set)"
-/zap/zap-baseline.py -s -t https://www.example.com/ --auto -c configs/baseline2.conf > /zap/wrk/output/baseline2.out
+echo "TEST: Baseline test 2 (vs zaproxy.com with INFO/WARN/FAIL set)"
+/zap/zap-baseline.py -s -t https://www.zaproxy.com/ --auto -c configs/baseline2.conf > /zap/wrk/output/baseline2.out
 check_results "baseline2" $? 1
 
 echo
-echo "TEST: Baseline test 3 (vs example.com with INFO/WARN/FAIL and OUTOFSCOPE set via file)"
-/zap/zap-baseline.py -s -t https://www.example.com/ -c configs/baseline3.conf > /zap/wrk/output/baseline3.out
+echo "TEST: Baseline test 3 (vs zaproxy.com with INFO/WARN/FAIL and OUTOFSCOPE set via file)"
+/zap/zap-baseline.py -s -t https://www.zaproxy.com/ -c configs/baseline3.conf > /zap/wrk/output/baseline3.out
 check_results "baseline3" $? 1
 
 echo
-echo "TEST: Baseline test 4 (vs example.com with INFO/WARN/FAIL and OUTOFSCOPE set via URL)"
-/zap/zap-baseline.py -s -t https://www.example.com/ -u https://raw.githubusercontent.com/zaproxy/zaproxy/main/docker/integration_tests/configs/baseline3.conf > /zap/wrk/output/baseline4.out
+echo "TEST: Baseline test 4 (vs zaproxy.com with INFO/WARN/FAIL and OUTOFSCOPE set via URL)"
+/zap/zap-baseline.py -s -t https://www.zaproxy.com/ -u https://raw.githubusercontent.com/zaproxy/zaproxy/main/docker/integration_tests/configs/baseline3.conf > /zap/wrk/output/baseline4.out
 check_results "baseline4" $? 1
 
 echo
 echo "TEST Baseline 5 (new vs old) - we expect _some_ differences and this will not fail the whole script"
-/zap/zap-baseline.py -s -t https://www.example.com/ --autooff > /zap/wrk/output/baseline1-orig.out
+/zap/zap-baseline.py -s -t https://www.zaproxy.com/ --autooff > /zap/wrk/output/baseline1-orig.out
 sed -i 's/ x . / x X /g' /zap/wrk/output/baseline1-orig.out
 DIFF=$(diff /zap/wrk/output/baseline1.out /zap/wrk/output/baseline1-orig.out) 
 echo "Differences:"
